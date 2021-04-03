a1a6fefa7d0085879a341d5641351cde21eba947
Wingman <ryuash123@gmail.com>
2021-04-03 23:23:44 +0800

    add: initial loading

--- src/screens/initial_load/index.tsx
+++ src/screens/initial_load/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import BigDipperLogoWhite from '@assets/big-dipper-white.svg';
+import BigDipperLogoRed from '@assets/big-dipper-red.svg';
+import { useSettingsContext } from '@contexts';
+import { LinearProgress } from '@material-ui/core';
+import { useStyles } from './styles';
 
 const InitialLoad = () => {
+  const { theme } = useSettingsContext();
+  const classes = useStyles();
   return (
-    <div>initial load</div>
+    <div className={classes.root}>
+      <div>
+        {theme === 'light' ? (
+          <BigDipperLogoRed />
+        ) : (
+          <BigDipperLogoWhite />
+        )}
+        <LinearProgress className={classes.divider} />
+        <img src="/logo-desmos.png" className={classes.logo} alt="logo" />
+      </div>
+    </div>
   );
 };
 
