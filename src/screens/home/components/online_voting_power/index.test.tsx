66a80d35fc0a7dccb764a2ea448ac814c2da5349
ryuash <ryuash123@gmail.com>
2021-04-13 17:16:37 +0800

    integrate: online voting power

--- src/screens/home/components/online_voting_power/index.test.tsx
+++ src/screens/home/components/online_voting_power/index.test.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { MockTheme } from '@tests/utils';
+import { ApolloProvider } from '@apollo/client';
+import {
+  createMockClient, createMockSubscription,
+} from 'mock-apollo-client';
+import {
+  OnlineVotingPowerListenerDocument,
+  TokenomicsDocument,
+} from '@graphql/types';
+import {
+  MockTheme, wait,
+} from '@tests/utils';
 import OnlineVotingPower from '.';
 
 // ==================================
@@ -15,17 +25,89 @@ jest.mock('@components', () => ({
   Box: (props) => <div id="box" {...props} />,
 }));
 
+const mockOnlineVotingPower = {
+  data: {
+    block: [
+      {
+        height: 1143207,
+        preCommitsAggregate: {
+          aggregate: {
+            sum: {
+              votingPower: 257518,
+            },
+          },
+        },
+      },
+    ],
+  },
+};
+
+const mockTokenomics = jest.fn().mockResolvedValue({
+  data: {
+    stakingPool: [
+      {
+        bonded: 254578529800,
+        unbonded: 204887435198,
+      },
+    ],
+    supply: [
+      {
+        coins: [
+          {
+            denom: 'udaric',
+            amount: '7987725829900',
+          },
+          {
+            denom: 'upotin',
+            amount: '80000000000000',
+          },
+        ],
+      },
+    ],
+  },
+});
+
 // ==================================
 // unit tests
 // ==================================
 describe('screen: Home/OnlineVotingPower', () => {
-  it('matches snapshot', () => {
-    const component = renderer.create(
-      <MockTheme>
-        <OnlineVotingPower />
-      </MockTheme>,
+  it('matches snapshot', async () => {
+    const mockClient = createMockClient();
+    const mockSubscription = createMockSubscription();
+
+    mockClient.setRequestHandler(
+      OnlineVotingPowerListenerDocument,
+      () => mockSubscription,
     );
-    const tree = component.toJSON();
+
+    mockClient.setRequestHandler(
+      TokenomicsDocument,
+      mockTokenomics,
+    );
+
+    let component;
+
+    renderer.act(() => {
+      component = renderer.create(
+        <ApolloProvider client={mockClient}>
+          <MockTheme>
+            <OnlineVotingPower />
+          </MockTheme>
+          ,
+        </ApolloProvider>,
+      );
+    });
+    await wait();
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    renderer.act(() => {
+      mockSubscription.next(mockOnlineVotingPower);
+    });
+    await wait();
+
+    tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
