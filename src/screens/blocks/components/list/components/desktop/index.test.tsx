9682a92da6abb5a5b1d2610edfd72c4f1ee8d6e4
ryuash <ryuash123@gmail.com>
2021-04-13 16:29:21 +0800

    update: testing

--- src/screens/blocks/components/list/components/desktop/index.test.tsx
+++ src/screens/blocks/components/list/components/desktop/index.test.tsx
@@ -12,7 +12,6 @@ const mockI18n = {
 };
 jest.mock('next-translate/useTranslation', () => () => mockI18n);
 jest.mock('@components', () => ({
-  AvatarName: (props) => <div id="AvatarName" {...props} />,
   Loading: (props) => <div id="Loading" {...props} />,
 }));
 
@@ -22,17 +21,15 @@ jest.mock('react-virtualized-auto-sizer', () => ({ children }: any) => children(
 
 jest.mock('@src/screens/blocks/components/list/contexts/blocks', () => ({
   useBlocksContext: () => ({
-    items: [{
-      height: '812,768,640',
-      proposer: {
-        image: 'https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg',
-        moniker: 'Forbole',
-        identity: 'FKsC411dik9ktS6xPADxs4Fk2SCENvAiuccQHLAPndvk',
+    formatUi: jest.fn(() => ([
+      {
+        height: <div>4,000</div>,
+        txs: 12,
+        time: 'moments ago',
+        proposer: <div>proposer</div>,
+        hash: '76nwV8zz8tLz97SBRXH6uwHvgHXtqJDLQfF66jZhQ857',
       },
-      hash: '76nwV8zz8tLz97SBRXH6uwHvgHXtqJDLQfF66jZhQ857',
-      tx: 2,
-      time: 1615187146246,
-    }],
+    ])),
     itemsCount: 2,
     loadMoreItems: jest.fn(),
     isItemLoaded: jest.fn(),
