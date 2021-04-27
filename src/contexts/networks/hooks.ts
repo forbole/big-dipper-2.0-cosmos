464c4bc58db5930de6240282e18cf3ad113ab3ba
Wingman L <42913823+ryuash@users.noreply.github.com>
2021-04-27 15:46:15 +0800

    update: network api (#25)

--- src/contexts/networks/hooks.ts
+++ src/contexts/networks/hooks.ts
@@ -10,7 +10,7 @@ import {
 import { NetworksState } from './types';
 
 export const useNetwork = () => {
-  const NETWORK_LIST_API = 'https://gist.githubusercontent.com/kwunyeung/8be4598c77c61e497dfc7220a678b3ee/raw/bd-networks.json';
+  const NETWORK_LIST_API = 'https://raw.githubusercontent.com/forbole/big-dipper-networks/main/networks.json';
 
   const [networks, setNetworks] = useState<BigDipperNetwork[]>([]);
   const [loading, setLoading] = useState(true);
