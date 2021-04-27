134d6ee1d66be6f2ffadd3294228f9fe72b3b203
Wingman L <42913823+ryuash@users.noreply.github.com>
2021-04-27 16:46:11 +0800

    Fix/testing (#26)

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,20 @@
+/* eslint-disable */
+
 require('@testing-library/jest-dom/extend-expect');
 require('jest-localstorage-mock');
+
+jest.mock('@utils/dayjs', () => {
+  const mockTest = () => ({
+    format: jest.fn(() => '2020-08-10 12:00:00'),
+  });
+
+  mockTest.utc = jest.fn(() => {
+    return (
+      {
+        fromNow: jest.fn(() => '1 day ago'),
+        format: jest.fn(() => '2020-08-10 12:00:00'),
+      }
+    );
+  });
+  return mockTest;
+});
