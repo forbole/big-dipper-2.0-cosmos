f55b845d12cd33ed7c70eff73ece5fe0a3fb35b9
Wingman <ryuash123@gmail.com>
2021-04-28 22:31:59 +0800

    fix: validator list self

--- src/screens/validators/components/list/contexts/validators/hooks.tsx
+++ src/screens/validators/components/list/contexts/validators/hooks.tsx
@@ -54,10 +54,6 @@ export const useValidators = (initialState: ValidatorsState) => {
     },
   });
 
-  useEffect(() => {
-    console.log(`tab has changed to ${tab}`);
-  }, [tab]);
-
   const formatValidators = (data: ValidatorsQuery) => {
     const votingPowerOverall = formatDenom(R.pathOr(0, ['stakingPool', 0, 'bondedTokens'], data));
     const signedBlockWindow = R.pathOr(0, ['slashingParams', 0, 'signedBlockWindow'], data);
@@ -65,22 +61,20 @@ export const useValidators = (initialState: ValidatorsState) => {
       const validator = x.validatorInfo.operatorAddress;
       const votingPower = R.pathOr(0, ['validatorVotingPowers', 0, 'votingPower'], x);
       const votingPowerPercent = numeral((votingPower / votingPowerOverall) * 100).value();
+      const totalDelegations = x.delegations.reduce((a, b) => {
+        return a + numeral(R.pathOr(0, ['amount', 'amount'], b)).value();
+      }, 0);
+
       const [selfDelegation] = x.delegations.filter(
         (y) => {
           return y.delegatorAddress === x.validatorInfo.selfDelegateAddress;
         },
       );
+      const self = numeral(R.pathOr(0, ['amount', 'amount'], selfDelegation)).value();
+      const selfPercent = (self / (totalDelegations || 1)) * 100;
 
-      const self = formatDenom(numeral(R.pathOr(0, ['amount', 'amount'], selfDelegation)).value());
-      const selfPercent = (self / (votingPower || 1)) * 100;
       const missedBlockCounter = R.pathOr(0, ['validatorSigningInfos', 0, 'missedBlocksCounter'], x);
       const condition = getValidatorCondition(signedBlockWindow, missedBlockCounter);
-
-      if (validator === 'desmosvaloper18kvwy5hzcu3ss08lcfcnx0eajuecg69uvk76c3') {
-        console.log(x, 'x');
-        console.log(self, 'self');
-        console.log(votingPower, 'votingPower');
-      }
 
       return ({
         moniker: findAddress(validator)?.moniker || validator,
