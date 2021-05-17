6348e13fa420bfd97bf7225af79d6b7b70a99051
ryuash <ryuash123@gmail.com>
2021-05-17 16:41:36 +0800

    .

--- src/screens/proposal_details/components/overview/index.test.tsx
+++ src/screens/proposal_details/components/overview/index.test.tsx
@@ -15,41 +15,41 @@ jest.mock('@components', () => ({
 
 jest.mock('../../contexts/proposal', () => ({
   useProposalContext: () => ({
-    item: {
-      id: 3,
-      proposer: {
-        image: 'https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg',
-        moniker: 'Forbole',
-        identity: 'FKsC411dik9ktS6xPADxs4Fk2SCENvAiuccQHLAPndvk',
+    tab: 0,
+    rawData: {
+      exists: true,
+      loading: true,
+      deposits: [],
+      votes: [],
+      voteTally: {
+        yes: 0,
+        no: 0,
+        abstain: 0,
+        veto: 0,
       },
-      type: 'text proposal',
-      title: 'Enable IBC transfers',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse placerat velit ipsum, tempus tristique tellus rhoncus tempus. Duis elementum varius neque, eu pharetra eros porttitor in.',
-      submissionTime: 1615187146246,
-      votingTimeStart: 1615187146246,
-      status: 1,
-      votes: [
-        {
-          voter: {
-            image: 'https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg',
-            moniker: 'Forbole',
-            identity: 'FKsC411dik9ktS6xPADxs4Fk2SCENvAiuccQHLAPndvk',
-          },
-          votingPower: '34,000,300',
-          vote: 2,
-        },
-      ],
-      deposits: [
-        {
-          depositor: {
-            image: 'https://s3.amazonaws.com/keybase_processed_uploads/f5b0771af36b2e3d6a196a29751e1f05_360_360.jpeg',
-            moniker: 'Forbole',
-            identity: 'FKsC411dik9ktS6xPADxs4Fk2SCENvAiuccQHLAPndvk',
-          },
-          amount: '12',
-          time: 1615187146246,
-        },
-      ],
+      overview: {
+        title: '',
+        id: 0,
+        description: '',
+        status: '',
+        submitTime: '',
+        depositEndTime: '',
+        votingEndTime: null,
+        votingStartTime: null,
+      },
+    },
+    uiData: {
+      overview: {
+        title: 'title',
+        id: '3',
+        description: 'description',
+        status: <div>status</div>,
+        submitTime: 'submitTime',
+        depositEndTime: 'depositEndTime',
+        votingEndTime: null,
+        votingStartTime: null,
+        type: 'type',
+      },
     },
   }),
 }));
