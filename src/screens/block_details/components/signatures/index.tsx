379070f126f86738bb68d29f44925f8974719101
Wingman <ryuash123@gmail.com>
2021-05-22 11:21:40 +0800

    more testing

--- src/screens/block_details/components/signatures/index.tsx
+++ src/screens/block_details/components/signatures/index.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
 import classnames from 'classnames';
 import useTranslation from 'next-translate/useTranslation';
 import { Typography } from '@material-ui/core';
 import {
   Box, NoData,
 } from '@components';
+import { useScreenSize } from '@hooks';
 import { useStyles } from './styles';
-import {
-  Desktop,
-  Mobile,
-} from './components';
 import { useBlockContext } from '../../contexts/block';
+
+const Desktop = dynamic(() => import('./components/desktop'));
+const Mobile = dynamic(() => import('./components/mobile'));
 
 const Signatures: React.FC<{
   className?: string;
 }> = ({ className }) => {
+  const { isDesktop } = useScreenSize();
   const { t } = useTranslation('blocks');
   const classes = useStyles();
   const {
@@ -28,8 +30,11 @@ const Signatures: React.FC<{
         <NoData />
       ) : (
         <div className={classes.wrapper}>
-          <Mobile className={classes.mobile} />
-          <Desktop className={classes.desktop} />
+          {isDesktop ? (
+            <Desktop className={classes.desktop} />
+          ) : (
+            <Mobile className={classes.mobile} />
+          )}
         </div>
       )}
     </Box>
