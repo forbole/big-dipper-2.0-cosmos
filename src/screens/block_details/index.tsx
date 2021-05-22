1ca33ba66f5a6e3d05ed7a0c84e89f944979e8e6
Wingman <ryuash123@gmail.com>
2021-05-22 17:44:04 +0800

    messed with dynamic import

--- src/screens/block_details/index.tsx
+++ src/screens/block_details/index.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
 import useTranslation from 'next-translate/useTranslation';
 import {
   Layout,
   NotFound,
   LinearLoading,
 } from '@components';
-import {
-  Overview,
-  Transactions,
-  Signatures,
-} from './components';
+// import {
+//   Overview,
+//   Transactions,
+//   Signatures,
+// } from './components';
 import { useStyles } from './styles';
 import { useBlockDetails } from './hooks';
+
+const Overview = dynamic(() => import('./components/overview'));
+const Transactions = dynamic(() => import('./components/transactions'));
+const Signatures = dynamic(() => import('./components/signatures'));
 
 const BlockDetails = () => {
   const { t } = useTranslation('blocks');
