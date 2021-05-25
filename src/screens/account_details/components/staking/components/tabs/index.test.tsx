8a6ab0c4f8eff654e7b30d411fc3b215063325d1
ryuash <ryuash123@gmail.com>
2021-05-25 11:38:42 +0800

    refactor: account details

--- src/screens/account_details/components/staking/components/tabs/index.test.tsx
+++ src/screens/account_details/components/staking/components/tabs/index.test.tsx
@@ -3,39 +3,6 @@ import renderer from 'react-test-renderer';
 import { MockTheme } from '@tests/utils';
 import TabsHeader from '.';
 
-jest.mock('../../../../contexts/account', () => ({
-  useAccountContext: () => {
-    return ({
-      uiData: {
-        staking: {
-          delegations: Array(4).fill(null),
-          redelegations: Array(14).fill(null),
-          unbondings: Array(4).fill(null),
-        },
-      },
-    });
-  },
-}));
-
-jest.mock('../../utils', () => ({
-  getTabs: jest.fn(() => ([
-    {
-      id: 0,
-      key: 'delegations',
-      component: jest.fn(() => <div id="delegations" />),
-    },
-    {
-      id: 1,
-      key: 'redelegations',
-      component: jest.fn(() => <div id="delegations" />),
-    },
-    {
-      id: 2,
-      key: 'unbondings',
-      component: jest.fn(() => <div id="delegations" />),
-    },
-  ])),
-}));
 // ==================================
 // unit tests
 // ==================================
@@ -43,7 +10,18 @@ describe('screen: Validators/Tabs', () => {
   it('matches snapshot', () => {
     const component = renderer.create(
       <MockTheme>
-        <TabsHeader tab={0} handleTabChange={jest.fn()} />
+        <TabsHeader
+          tab={0}
+          handleTabChange={jest.fn()}
+          tabs={[
+            {
+              id: 0,
+              key: 'key',
+              count: 1,
+              component: <div id="component" />,
+            },
+          ]}
+        />
       </MockTheme>,
     );
     const tree = component.toJSON();
