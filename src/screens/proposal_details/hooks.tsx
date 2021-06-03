4b8cbc75fb84041402f6a53b430d9919cfa750ad
Wingman L <42913823+ryuash@users.noreply.github.com>
2021-06-03 17:12:52 +0800

    Fix/proposals pagination (#95)

--- src/screens/proposal_details/hooks.tsx
+++ src/screens/proposal_details/hooks.tsx
@@ -192,7 +192,7 @@ export const useProposalDetails = () => {
         },
         vote: x.option,
       });
-    });
+    }).sort((a, b) => ((a.user.name > b.user.name) ? 1 : -1));
 
     return {
       data: votes,
