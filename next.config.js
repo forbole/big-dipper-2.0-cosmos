33f76f04e1a13017660e4c9fb8209d791dc3d7b6
ryuash <ryuash123@gmail.com>
2021-06-16 17:07:44 +0800

    add: helmet

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const nextTranslate = require('next-translate');
 
 module.exports = nextTranslate({
+  poweredByHeader: false,
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
