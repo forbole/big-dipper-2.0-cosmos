332b8a3bf31ff1ae56b44869f3d2fb3a3de18d42
ryuash <ryuash123@gmail.com>
2021-06-25 13:16:42 +0800

    add: latest block chain time feature

--- src/screens/home/hooks.ts
+++ src/screens/home/hooks.ts
@@ -1,37 +1,28 @@
-// console.log(data, 'data');
-//       const timestamp = R.pathOr('', ['height', 0, 'timestamp'], data.subscriptionData.data);
-//       dayjs.utc(timestamp).fromNow();
 import dayjs from '@utils/dayjs';
-import { chainConfig } from '@configs';
 import { toast } from 'react-toastify';
 import { useState } from 'react';
+import useTranslation from 'next-translate/useTranslation';
 import * as R from 'ramda';
-import {
-  useLatestBlockTimestampQuery,
-  LatestBlockTimestampQuery,
-} from '@graphql/types';
+import { useLatestBlockTimestampQuery } from '@graphql/types';
 
 export const useHome = () => {
+  const { t } = useTranslation('common');
   const [chainActive, setChainActive] = useState(true);
 
   useLatestBlockTimestampQuery({
     onCompleted: (data) => {
-      const timestamp = dayjs.utc('2021-06-25T03:23:27.762512');
-      // dayjs.utc(data?.block[0]?.timestamp);
+      const timestamp = dayjs.utc(R.pathOr('', ['block', 0, 'timestamp'], data));
       const timeNow = dayjs.utc();
       const timeDifference = timeNow.diff(timestamp, 's');
-      // const timeDifferenceMinutes = timeNow.diff(timestamp, 'm');
-      console.log(timeNow.diff(timestamp, 's'), 'diff');
       // if latest block has been over a minute ago
-      if (timeDifference > 60) {
-        toast(`latest block was ${dayjs.utc(timestamp).fromNow()}`);
+      if (timeDifference > 60 && chainActive) {
+        toast.error(t('blockTimeAgo', {
+          time: dayjs.utc(timestamp).fromNow(),
+        }), {
+          autoClose: false,
+        });
+        setChainActive(false);
       }
-      // data?.block[0]?.timestamp;
-
-      // setState((prevState) => ({
-      //   ...prevState,
-      //   blockTime: formatAverageBlockTime(data),
-      // }));
     },
   });
 };
