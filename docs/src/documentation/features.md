090e0b43f42b86254d2f6bb3587397718d0f076e
Wingman L <42913823+ryuash@users.noreply.github.com>
2021-07-30 17:27:02 +0800

    seting up desmos v1.0.3 (#163)

--- docs/src/documentation/features.md
+++
@@ -1,2 +0,0 @@
-## Overview
-Below are a list of features we currently support
--- src/screens/validator_details/index.tsx
+++ src/screens/validator_details/index.tsx
@@ -58,6 +58,7 @@ const ValidatorDetails = () => {
                 commission: overview.commission,
                 signedBlockWindow: overview.signedBlockWindow,
                 missedBlockCounter: overview.missedBlockCounter,
+                lastSeen: overview.lastSeen,
               }}
             />
           ) : (
@@ -74,6 +75,7 @@ const ValidatorDetails = () => {
               commission={overview.commission}
               signedBlockWindow={overview.signedBlockWindow}
               missedBlockCounter={overview.missedBlockCounter}
+              lastSeen={overview.lastSeen}
             />
           )}
           <VotingPower
