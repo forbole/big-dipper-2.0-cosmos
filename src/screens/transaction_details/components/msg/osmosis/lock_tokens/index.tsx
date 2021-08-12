6c37470755b7b203f232ee52882f8aecd481f528
Magic Cat <pusz.monika@gmail.com>
2021-08-12 17:47:55 +0100

    Add lock tokens msg in screens

---
+++ src/screens/transaction_details/components/msg/osmosis/lock_tokens/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Trans from 'next-translate/Trans';
+import { Typography } from '@material-ui/core';
+import { Name } from '@components';
+import { MsgLockTokens } from '@models';
+import { useChainContext } from '@contexts';
+import numeral from 'numeral';
+import { chainConfig } from '@configs';
+import { msToTime } from '@utils/dayjs';
+
+const LockTokens = (props: {
+    message: MsgLockTokens;
+}) => {
+  const { findAddress } = useChainContext();
+  const { message } = props;
+  const owner = findAddress(message.owner);
+  const ownerMoniker = owner ? owner?.moniker : message.owner;
+  return (
+    <Typography>
+      <Trans
+        i18nKey="message_contents:txLockTokensContent"
+        components={[
+          (
+            <Name
+              address={message.owner}
+              name={ownerMoniker}
+            />
+          ),
+        ]}
+        values={{
+          amount: message?.coins?.map((x) => {
+            return `${numeral(parseFloat(x?.amount) / 10 ** chainConfig.tokenUnits[x?.denom]?.exponent).format('0,0.[0000]')} ${chainConfig.tokenUnits[x?.denom]?.display?.toUpperCase()}`;
+          }),
+          duration: msToTime(message?.duration.substring(0, message?.duration.length() - 1) * 1000),
+        }}
+      />
+    </Typography>
+  );
+};
+
+export default LockTokens;
