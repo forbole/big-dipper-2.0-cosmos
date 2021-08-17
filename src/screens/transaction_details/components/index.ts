78ef41ff209f899f4f32b4d780bee22c43fda3ca
Magic Cat <pusz.monika@gmail.com>
2021-08-17 14:06:39 +0100

    Updated message types

--- src/screens/transaction_details/components/index.ts
+++ src/screens/transaction_details/components/index.ts
@@ -26,6 +26,32 @@ import DtagCancelTransfer from './msg/profiles/dtag_cancel_transfer';
 import DtagRefuseTransfer from './msg/profiles/dtag_refuse_transfer';
 import BlockUser from './msg/profiles/block_user';
 import UnBlockUser from './msg/profiles/unblock_user';
+import CreateClient from './msg/client/create_client';
+import UpdateClient from './msg/client/update_client';
+import UpgradeClient from './msg/client/upgrade_client';
+import SubmitMisbehaviour from './msg/client/submit_misbehaviour';
+import Height from './msg/client/height';
+import Acknowledgement from './msg/channel/acknowledgement';
+import Channel from './msg/channel/channel';
+import ChannelCloseConfirm from './msg/channel/channel_close_confirm';
+import ChannelCloseInit from './msg/channel/channel_close_init';
+import ChannelOpenAck from './msg/channel/channel_open_ack';
+import ChannelOpenConfirm from './msg/channel/channel_open_confirm';
+import ChannelOpenInit from './msg/channel/channel_open_init';
+import ChannelOpenTry from './msg/channel/channel_open_try';
+import CounterpartyChannel from './msg/channel/counterparty';
+import Packet from './msg/channel/packet';
+import ReceivePacket from './msg/channel/receive_packet';
+import Timeout from './msg/channel/timeout';
+import TimeoutOnClose from './msg/channel/timeout_on_close';
+import ConnectionEnd from './msg/connection/connection_end';
+import ConnectionOpenAck from './msg/connection/connection_open_ack';
+import ConnectionOpenConfirm from './msg/connection/connection_open_confirm';
+import ConnectionOpenInit from './msg/connection/connection_open_init';
+import ConnectionOpenTry from './msg/connection/connection_open_try';
+import CounterpartyConnection from './msg/connection/counterparty';
+import Version from './msg/connection/version';
+import Transfer from './msg/transfer/transfer';
 
 export {
   Overview,
@@ -60,4 +86,30 @@ export {
   DtagRefuseTransfer,
   BlockUser,
   UnBlockUser,
+  CreateClient,
+  UpdateClient,
+  UpgradeClient,
+  SubmitMisbehaviour,
+  Height,
+  Acknowledgement,
+  Channel,
+  ChannelCloseConfirm,
+  ChannelCloseInit,
+  ChannelOpenAck,
+  ChannelOpenConfirm,
+  ChannelOpenInit,
+  ChannelOpenTry,
+  CounterpartyChannel,
+  Packet,
+  ReceivePacket,
+  Timeout,
+  TimeoutOnClose,
+  ConnectionEnd,
+  ConnectionOpenAck,
+  ConnectionOpenConfirm,
+  ConnectionOpenInit,
+  ConnectionOpenTry,
+  CounterpartyConnection,
+  Version,
+  Transfer,
 };
