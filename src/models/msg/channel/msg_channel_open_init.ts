46eed4669893a8ac6e60152bca4cc7b38a0bb24b
Magic Cat <pusz.monika@gmail.com>
2021-08-17 13:51:42 +0100

    Implemented ibc channel messages

---
+++ src/models/msg/channel/msg_channel_open_init.ts
@@ -0,0 +1,31 @@
+import { Categories } from '../types';
+
+class MsgChannelOpenInit {
+    public category: Categories;
+    public type: string;
+    public signer: string;
+    public channel: string;
+    public portId: string;
+    public json: any;
+
+    constructor(payload: any) {
+      this.category = 'channel';
+      this.type = payload.type;
+      this.signer = payload.signer;
+      this.channel = payload.channel;
+      this.portId = payload.portId;
+      this.json = payload.json;
+    }
+
+    static fromJson(json: any) {
+      return new MsgChannelOpenInit({
+        json,
+        type: json['@type'],
+        signer: json.signer,
+        channel: json.channel,
+        portId: json.port_id,
+      });
+    }
+}
+
+export default MsgChannelOpenInit;
---
+++ src/screens/transaction_details/components/msg/channel/timeout_on_close/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Trans from 'next-translate/Trans';
+import { Typography } from '@material-ui/core';
+import { Name } from '@components';
+import { MsgTimeoutOnClose } from '@models';
+import { useChainContext } from '@contexts';
+
+const TimeoutOnClose = (props: {
+  message: MsgTimeoutOnClose;
+}) => {
+  const { findAddress } = useChainContext();
+  const { message } = props;
+
+  const signer = findAddress(message.signer);
+  const signerMoniker = signer ? signer?.moniker : message.signer;
+
+  return (
+    <Typography>
+      <Trans
+        i18nKey="message_contents:txTimeoutOnCloseContent"
+        components={[
+          (
+            <Name
+              address={message.signer}
+              name={signerMoniker}
+            />
+          ),
+          <b />,
+        ]}
+      />
+    </Typography>
+  );
+};
+
+export default TimeoutOnClose;
