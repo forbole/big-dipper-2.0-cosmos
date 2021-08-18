6eb9e11296aaf65586698f988dcd3538c4ecba45
ryuash <ryuash123@gmail.com>
2021-08-18 11:20:08 +0800

    add: tests to crisis module

---
+++ src/screens/transaction_details/components/msg/crisis/verify_invariant/index.test.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { MockTheme } from '@tests/utils';
+import { MsgVerifyInvariant } from '@models';
+import VerifyInvariant from '.';
+
+// ==================================
+// mocks
+// ==================================
+jest.mock('@contexts', () => ({
+  useChainContext: () => ({
+    findAddress: jest.fn(() => ({
+      moniker: 'moniker',
+      imageUrl: null,
+    })),
+  }),
+}));
+
+jest.mock('@components', () => ({
+  Name: (props) => <div id="Name" {...props} />,
+}));
+
+// ==================================
+// unit tests
+// ==================================
+describe('screen: TransactionDetails/MsgSend', () => {
+  it('matches snapshot', () => {
+    const message = new MsgVerifyInvariant({
+      category: 'crisis',
+      type: 'MsgVerifyInvariant',
+      sender: 'sender',
+      invariantModuleName: 'invariantModuleName',
+      invariantRoute: 'invariantRoute',
+    });
+    const component = renderer.create(
+      <MockTheme>
+        <VerifyInvariant
+          message={message}
+        />
+      </MockTheme>,
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+});
