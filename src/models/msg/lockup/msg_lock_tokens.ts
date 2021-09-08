9812f4018aaf457071f3b778cb55be63909c6710
ryuash <ryuash123@gmail.com>
2021-09-08 15:20:40 +0800

    .

--- src/models/msg/lockup/msg_lock_tokens.ts
+++ src/models/msg/lockup/msg_lock_tokens.ts
@@ -28,7 +28,7 @@ class MsgLockTokens {
         type: json['@type'],
         owner: json.owner,
         duration: json.duration,
-        coins: json?.coins.map((x) => {
+        coins: R.pathOr([], ['coins'], json).map((x) => {
           return ({
             denom: R.pathOr('', ['coins', 'denom'], x),
             amount: numeral(R.pathOr('0', ['coins', 'amount'], x)).value(),
