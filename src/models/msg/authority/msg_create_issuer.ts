3a32c3f59106350ad6b69f4febbfdb9cc48ea6d1
Ellie <isabelleya927@gmail.com>
2021-09-14 16:19:47 +0800

    revise msg_create_issuer.ts file

--- src/models/msg/authority/msg_create_issuer.ts
+++ src/models/msg/authority/msg_create_issuer.ts
@@ -6,44 +6,26 @@ class MsgCreateIssuer {
     public type: string;
     public json: any;
 
-    public owner: string;
-    public clientOrderId: string;
-    public timeInForce: 'Unspecified' | 'GoodTillCancel' | 'ImmediateOrCancel' | 'FillOrKill';
-    public source: {
-      amount: number;
-      denom: string;
-    };
-    public destination: {
-      amount: number;
-      denom: string;
-    };
+    public authority: string;
+    public issuer: string;
+    public denominations: array;
 
     constructor(payload: any) {
       this.category = 'authority';
       this.type = payload.type;
       this.json = payload.json;
-      this.owner = payload.owner;
-      this.clientOrderId = payload.clientOrderId;
-      this.timeInForce = payload.timeInForce;
-      this.source = payload.source;
-      this.destination = payload.destination;
+      this.authority = payload.authority;
+      this.issuer = payload.issuer;
+      this.denominations = payload.denominations;
     }
 
     static fromJson(json: any) {
       return new MsgCreateIssuer({
         json,
         type: json['@type'],
-        owner: json.owner,
-        clientOrderId: R.pathOr('', ['client_order_id'], json),
-        timeInForce: R.pathOr('Unspecified', ['time_in_force'], json),
-        source: {
-          amount: R.pathOr(0, ['source', ' amount'], json),
-          denom: R.pathOr(0, ['source', ' denom'], json),
-        },
-        destination: {
-          amount: R.pathOr(0, ['source', ' amount'], json),
-          denom: R.pathOr(0, ['source', ' denom'], json),
-        },
+        authority: json.authority,
+        issuer: R.pathOr('', ['issuer'], json),
+        denominations: R.pathOr(0, ['denominations'], json),
       });
     }
 }
