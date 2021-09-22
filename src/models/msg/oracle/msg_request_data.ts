6e58b3c82056593e12d0db71a92d7420490dbcd7
Wingman <ryuash123@gmail.com>
2021-09-22 17:09:29 +0800

    wip: set models

--- src/models/msg/oracle/msg_request_data.ts
+++ src/models/msg/oracle/msg_request_data.ts
@@ -10,7 +10,10 @@ class MsgRequestData {
   public askCount: number;
   public minCount: number;
   public clientId: string;
-  public feeLimit: MsgCoins[];
+  public feeLimit: MsgCoin[];
+  public prepareGas: number;
+  public executeGas: number;
+  public sender: string;
 
   constructor(payload: any) {
     this.category = 'oracle';
@@ -21,6 +24,10 @@ class MsgRequestData {
     this.askCount = payload.askCount;
     this.minCount = payload.minCount;
     this.clientId = payload.clientId;
+    this.feeLimit = payload.feeLimit;
+    this.prepareGas = payload.prepareGas;
+    this.executeGas = payload.executeGas;
+    this.sender = payload.sender;
   }
 
   static fromJson(json: any) {
@@ -31,6 +38,14 @@ class MsgRequestData {
       calldata: R.pathOr('', ['calldata'], json),
       askCount: R.pathOr(0, ['ask_count'], json),
       minCount: R.pathOr(0, ['min_count'], json),
+      clientId: R.pathOr('', ['client_id'], json),
+      feeLimit: R.pathOr([], ['fee_limit'], json).map((x) => ({
+        denom: x.denom,
+        amount: R.pathOr(0, ['amount'], x),
+      })),
+      prepareGas: R.pathOr(0, ['prepare_gas'], json),
+      executeGas: R.pathOr(0, ['executeGas'], json),
+      sender: R.pathOr('', ['sender'], json),
     });
   }
 }
