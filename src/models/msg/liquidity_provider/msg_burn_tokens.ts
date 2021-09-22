723998e59bd57863fb5986dfa0c1a2c08705d8f2
Ellie <isabelleya927@gmail.com>
2021-09-22 17:58:25 +0800

    update model msg liquidityProvider

---
+++ src/models/msg/liquidity_provider/msg_burn_tokens.ts
@@ -0,0 +1,29 @@
+import * as R from 'ramda';
+import { Categories } from '../types';
+
+class MsgBurnTokens {
+    public category: Categories;
+    public type: string;
+    public json: any;
+    public liquidityProvider: string;
+    public amount: string;
+
+    constructor(payload: any) {
+      this.category = 'liquidityProvider';
+      this.type = payload.type;
+      this.json = payload.json;
+      this.liquidityProvider = payload.liquidityProvider;
+      this.amount = payload.amount;
+    }
+
+    static fromJson(json: any) {
+      return new MsgBurnTokens({
+        json,
+        type: json['@type'],
+        liquidityProvider: json.liquidityProvider,
+        issuer: R.pathOr('', ['issuer'], json),
+      });
+    }
+}
+
+export default MsgBurnTokens;
