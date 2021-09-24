1da64c1e3724fed07a73c9b59da75f0bbdef38fd
Ellie <isabelleya927@gmail.com>
2021-09-24 10:19:35 +0800

    update message_contents

--- src/models/msg/liquidity_provider/msg_mint_tokens.ts
+++ src/models/msg/liquidity_provider/msg_mint_tokens.ts
@@ -12,7 +12,7 @@ class MsgMintTokens {
       this.category = 'liquidityProvider';
       this.type = payload.type;
       this.json = payload.json;
-      this.liquidityProvider = payload.liquidityProvider;
+      this.liquidityProvider = payload.liquidity_provider;
       this.amount = payload.amount;
     }
 
@@ -20,7 +20,7 @@ class MsgMintTokens {
       return new MsgMintTokens({
         json,
         type: json['@type'],
-        liquidityProvider: json.liquidityProvider,
+        liquidityProvider: json.liquidity_provider,
         amount: R.pathOr([], ['amount'], json).map((x) => {
           return ({
             denom: R.pathOr('', ['denom'], x),
