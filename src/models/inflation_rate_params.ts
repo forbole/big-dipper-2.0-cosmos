1638a5a3f1ec297ba1a602a310cd4d236d0ed10e
Ellie <isabelleya927@gmail.com>
2021-09-29 13:25:14 +0800

    update inflation_rate_params

--- src/models/inflation_rate_params.ts
+++ src/models/inflation_rate_params.ts
@@ -1,28 +1,18 @@
 import * as R from 'ramda';
-import { chainConfig } from '@configs';
 
 class InflationRateParams {
-  public bondDenom: string;
-  public unbondingTime: number;
-  public maxEntries: number;
-  public historicalEntries: number;
-  public maxValidators: number;
+  public denom: string;
+  public inflation: string;
 
   constructor(payload: any) {
-    this.bondDenom = payload.bondDenom;
-    this.unbondingTime = payload.unbondingTime;
-    this.maxEntries = payload.maxEntries;
-    this.historicalEntries = payload.historicalEntries;
-    this.maxValidators = payload.maxValidators;
+    this.denom = payload.denom;
+    this.inflation = payload.inflation;
   }
 
   static fromJson(data: any) {
     return new InflationRateParams({
-      bondDenom: R.pathOr(chainConfig.primaryTokenUnit, ['bond_denom'], data),
-      unbondingTime: R.pathOr(0, ['unbonding_time'], data),
-      maxEntries: R.pathOr(0, ['max_entries'], data),
-      historicalEntries: R.pathOr(0, ['historical_entries'], data),
-      maxValidators: R.pathOr(0, ['max_validators'], data),
+      denom: R.pathOr(0, ['denom'], data),
+      inflation: R.pathOr(0, ['inflation'], data),
     });
   }
 }
