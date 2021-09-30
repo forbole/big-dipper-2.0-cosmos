13d6de92accba430f3a01e95304267536442c2b7
Ellie <isabelleya927@gmail.com>
2021-09-30 11:38:48 +0800

    fix array default type

--- src/screens/params/hooks.tsx
+++ src/screens/params/hooks.tsx
@@ -144,7 +144,7 @@ export const useParams = () => {
 
     const formatGov = () => {
       if (data.govParams.length) {
-        const govParamsRaw = GovParams.fromJson(R.pathOr({}, ['govParams', 0], data));
+        const govParamsRaw = GovParams.fromJson(R.pathOr([], ['govParams', 0], data));
         return {
           minDeposit: formatDenom(
             R.pathOr(0, [0, 'amount'], govParamsRaw.depositParams.minDeposit),
