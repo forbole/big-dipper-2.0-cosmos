df0947a30cb61f889e35cc6f1b951df145139eed
Ellie <isabelleya927@gmail.com>
2021-10-13 10:33:54 +0800

    add account/address path

---
+++ src/pages/account/[address].tsx
@@ -0,0 +1,19 @@
+import AccountDetails from '@src/screens/account_details';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+const AccountDetailsPage = () => {
+  const router = useRouter();
+
+  console.log('router=>', router);
+
+  useEffect(() => {
+    router.replace(`/accounts/${router.query.address}`);
+  }, []);
+
+  return (
+    <AccountDetails />
+  );
+};
+
+export default AccountDetailsPage;
--- src/pages/validator/[address].tsx
+++ src/pages/validator/[address].tsx
@@ -5,10 +5,6 @@ import { useRouter } from 'next/router';
 const ValidatorDetailsPage = () => {
   const router = useRouter();
 
-  console.log('router=>', router);
-  console.log('router.asPath=>', router.asPath);
-  //   console.log('`validator/${router.query.address}`=>', `validator/${router.query.address}`);
-
   useEffect(() => {
     router.replace(`/validators/${router.query.address}`);
   }, []);
