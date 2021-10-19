c0c228cfd6eb5f1ac796cdd20047656a52a2a8cf
Ellie <isabelleya927@gmail.com>
2021-10-19 15:38:01 +0800

    update components/issue_denom UI

--- src/components/msg/nft/issue_denom/index.tsx
+++ src/components/msg/nft/issue_denom/index.tsx
@@ -9,13 +9,12 @@ import { useChainContext } from '@contexts';
 const IssueDenom = (props: {
   message: MsgIssueDenom ;
 }) => {
-  console.log('props', props);
-
   const { findAddress } = useChainContext();
   const { message } = props;
   const { t } = useTranslation('transactions');
   const { creators } = message;
-  const creatorsAddressResult = creators.reduce((text, value, i, array) => text + (i < array.length - 1 ? ', ' : ` ${t(' and ')}`) + value);
+
+  const creatorsAddressResult = creators.reduce((text, value, i, array) => text + (i < array.length - 1 ? ', ' : ` ${t('and')} `) + value);
 
   console.log('creatorsAddressResult => ', creatorsAddressResult);
 
@@ -24,25 +23,57 @@ const IssueDenom = (props: {
     const creatorMonikerResult = creatorMoniker ? creatorMoniker?.moniker : x;
     return creatorMonikerResult;
   });
-  const creatorsMonikerResult = creatorsMoniker.reduce((text, value, i, array) => text + (i < array.length - 1 ? ', ' : ` ${t(' and ')} `) + value);
+  const creatorsMonikerResult = creatorsMoniker.reduce((text, value, i, array) => text + (i < array.length - 1 ? ', ' : ` ${t('and')} `) + value);
 
   console.log('creatorsMonikerResult => ', creatorsMonikerResult);
 
+  const dataArray = creators.map((eachAddress) => {
+    const creatorMoniker = findAddress(eachAddress);
+    const creatorMonikerResult = creatorMoniker ? creatorMoniker?.moniker : eachAddress;
+    return {
+      eachAddress, creatorMonikerResult,
+    };
+  });
+
+  console.log('dataArray => ', dataArray);
+
   return (
-    <Typography>
-      <Trans
-        i18nKey="message_contents:txIssueDenomContent"
-        components={[
-          (
-            <Name
-              address={creatorsAddressResult}
-              name={creatorsMonikerResult}
-            />
-          ),
-          <b />,
-        ]}
-      />
-    </Typography>
+    // <Typography>
+    //   <Trans
+    //     i18nKey="message_contents:txIssueDenomContent"
+    //     components={[
+    //       (
+    //         <Name
+    //           address={creatorsAddressResult}
+    //           name={creatorsMonikerResult}
+    //         />
+    //       ),
+    //       <b />,
+    //     ]}
+    //   />
+    // </Typography>
+    <>
+      {
+        dataArray.map((x) => {
+          return (
+            <Typography>
+              <Trans
+                i18nKey="message_contents:txIssueDenomContent"
+                components={[
+                  (
+                    <Name
+                      address={x.eachAddress}
+                      name={x.creatorMonikerResult}
+                    />
+                  ),
+                  <b />,
+                ]}
+              />
+            </Typography>
+          );
+        })
+    }
+    </>
   );
 };
 
