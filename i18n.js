fa5c7cf416ff67bea5493ec5ff79e3e443f4aec7
Ellie <isabelleya927@gmail.com>
2021-10-21 09:54:55 +0800

    update i18n

--- i18n.js
+++ i18n.js
@@ -3,7 +3,7 @@ module.exports = {
   locales: ['en'],
   defaultLocale: 'en',
   pages: {
-    '*': ['common', 'message_labels', 'message_contents', 'transactions'],
+    '*': ['common'],
     '/': ['home', 'blocks', 'transactions'],
     'rgx:^/blocks': ['blocks', 'transactions', 'message_labels', 'message_contents'],
     'rgx:^/transactions': ['transactions', 'message_labels', 'message_contents'],
