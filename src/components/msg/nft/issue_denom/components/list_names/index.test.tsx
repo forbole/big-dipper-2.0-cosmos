ac011c76f724946055adb9a4c2f9192c29884684
Ellie <isabelleya927@gmail.com>
2021-10-22 18:46:21 +0800

    add new file for listNames testing

--- src/components/msg/nft/issue_denom/components/list_names/index.test.tsx
+++ src/components/msg/nft/issue_denom/components/list_names/index.test.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import TestRenderer from 'react-test-renderer';
 import { MsgIssueDenom } from '@models';
+import {
+  render, screen,
+} from '@testing-library/dom';
 import ListNames from '.';
 
 // ==================================
@@ -23,17 +27,19 @@ jest.mock('@components', () => ({
 // unit tests
 // ==================================
 describe('screen: TransactionDetails/IssueDenom', () => {
+  const message = new MsgIssueDenom({
+    category: 'nft',
+    type: 'MsgIssueDenom',
+    creators: [
+      'desmos1hfhkduejung7g29wv863x369rndf3hu5xj4g93',
+      'desmosvaloper14nfk5gm99gfrd7nwqtmtvzunzclz8720a6cqh7',
+      'desmosvaloper1jh753mzjy358jf86cfqqzkrrtqqefhjxctcre4',
+      'desmosvaloper1rzhewpmmdl72lhnxj6zmxr4v94f522s4hyz467',
+    ],
+  });
+
+  // snapshot
   it('matches snapshot', () => {
-    const message = new MsgIssueDenom({
-      category: 'nft',
-      type: 'MsgIssueDenom',
-      creators: [
-        'desmos1hfhkduejung7g29wv863x369rndf3hu5xj4g93',
-        'desmosvaloper14nfk5gm99gfrd7nwqtmtvzunzclz8720a6cqh7',
-        'desmosvaloper1jh753mzjy358jf86cfqqzkrrtqqefhjxctcre4',
-        'desmosvaloper1rzhewpmmdl72lhnxj6zmxr4v94f522s4hyz467',
-      ],
-    });
     const component = renderer.create(
       <ListNames creators={message.creators} />,
     );
@@ -41,18 +47,50 @@ describe('screen: TransactionDetails/IssueDenom', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  it('matches moniker', () => {
-    const { findAddress } = useChainContext();
-    const creator1 = 'desmos1hfhkduejung7g29wv863x369rndf3hu5xj4g93';
-    const creator2 = 'desmosvaloper14nfk5gm99gfrd7nwqtmtvzunzclz8720a6cqh7';
-    const creator3 = 'desmosvaloper1jh753mzjy358jf86cfqqzkrrtqqefhjxctcre4';
-    const output1 = findAddress(creator1);
-    const output2 = findAddress(creator2);
-    const output3 = findAddress(creator3);
+  // findById
+  it('matches html', () => {
+    render(<ListNames creators={message.creators} />);
+    const div = screen.findByTestId('desmos1hfhkduejung7g29wv863x369rndf3hu5xj4g93');
 
-    expect(output1).toMatch('desmos1hfhkduejung7g29wv863x369rndf3hu5xj4g93');
-    expect(output2).toMatch('AC Validator 🚀');
-    expect(output3).toMatch('coolex');
+    const expectedHtml = (
+      'desmos1hfhkduejung7g29wv863x369rndf3hu5xj4g93'
+    );
+    expect(div).toMatch(expectedHtml);
+  });
+
+  // test on suffix
+  it('creators is an empty array', () => {
+    const creators1 = [];
+    const component1 = renderer.create(
+      <ListNames creators={creators1} />,
+    );
+    const tree = component1.toJSON();
+    const render1 = tree.props;
+    expect(render1).toMatch('');
+  });
+
+  it('creators is an array of one creator', () => {
+    const creators2 = ['desmosvaloper14nfk5gm99gfrd7nwqtmtvzunzclz8720a6cqh7'];
+    const component2 = renderer.create(
+      <ListNames creators={creators2} />,
+    );
+    const tree = component2.toJSON();
+    const render2 = tree.props;
+    expect(render2).toMatch('AC Validator 🚀');
+  });
+
+  it('creators is an array of three creators', () => {
+    const creators3 = [
+      'desmos1hfhkduejung7g29wv863x369rndf3hu5xj4g93',
+      'desmosvaloper14nfk5gm99gfrd7nwqtmtvzunzclz8720a6cqh7',
+      'desmosvaloper1jh753mzjy358jf86cfqqzkrrtqqefhjxctcre4',
+    ];
+    const component3 = renderer.create(
+      <ListNames creators={creators3} />,
+    );
+    const tree = component3.toJSON();
+    const render3 = tree.props;
+    expect(render3).toMatch('desmos1hfhkduejung7g29wv863x369rndf3hu5xj4g93, AC Validator 🚀 and coolex');
   });
 
   afterEach(() => {
