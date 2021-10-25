68e31921b54826cccface7ab0960b8bfdeb193ef
ryuash <ryuash123@gmail.com>
2021-10-25 15:48:24 +0800

    ima regret this

--- src/recoil/validators/hooks.ts
+++ src/recoil/validators/hooks.ts
@@ -1,5 +1,8 @@
 /* eslint-disable max-len */
-import { useRecoilState } from 'recoil';
+import {
+  useRecoilState,
+  useRecoilCallback,
+} from 'recoil';
 import * as R from 'ramda';
 import {
   useValidatorAddressesQuery,
@@ -9,6 +12,7 @@ import { chainConfig } from '@configs';
 import { useDesmosProfile } from '@hooks';
 import {
   writeValidator,
+  atomFamilyState,
 } from '@recoil/validators';
 import { writeProfile } from '@recoil/profiles';
 
@@ -28,58 +32,80 @@ export const useValidatorRecoil = () => {
     },
     onCompleted: async (data) => {
       formatValidatorsAddressList(data);
-      setProfiles(data);
+      // setProfiles(data);
     },
   });
 
-  const formatValidatorsAddressList = async (data: ValidatorAddressesQuery) => {
+  const formatValidatorsAddressList = useRecoilCallback(({
+    snapshot, set,
+  }) => async (data: ValidatorAddressesQuery) => {
+    data?.validator?.filter((x) => x.validatorInfo).forEach((x) => {
+      const validatorAddress = x.validatorInfo.operatorAddress;
+      const delegatorAddress = x.validatorInfo.selfDelegateAddress;
+      const { consensusAddress } = x.validatorInfo;
+      snapshot.getPromise(writeValidator(consensusAddress)).then((test) => {
+        console.log(test, 'num of cars');
+      });
+
+      set(atomFamilyState(consensusAddress), {
+        delegator: delegatorAddress,
+        validator: validatorAddress,
+      });
+    });
+  });
+
+  const xformatValidatorsAddressList = async (data: ValidatorAddressesQuery) => {
     // set initial consensus address dictionary
     data?.validator?.filter((x) => x.validatorInfo).forEach((x) => {
       const validatorAddress = x.validatorInfo.operatorAddress;
       const delegatorAddress = x.validatorInfo.selfDelegateAddress;
       const { consensusAddress } = x.validatorInfo;
-      const [_, setValidator] = useRecoilState(writeValidator(consensusAddress));
-
-      setValidator({
-        delegator: delegatorAddress,
-        validator: validatorAddress,
+      useRecoilCallback(({
+        snapshot, set,
+      }) => async () => {
+        const numItemsInCart = await snapshot.getPromise(writeValidator(consensusAddress));
+        console.log(numItemsInCart, 'wow wahts this');
+        set(atomFamilyState(consensusAddress), {
+          delegator: delegatorAddress,
+          validator: validatorAddress,
+        });
       });
     });
   };
 
-  const setProfiles = async (data: ValidatorAddressesQuery) => {
-    if (chainConfig.extra.profile) {
-      let profiles = [];
-      data?.validator?.filter((x) => x.validatorInfo).forEach((x) => {
-        const delegatorAddress = x.validatorInfo.selfDelegateAddress;
-        profiles.push(fetchDesmosProfile(delegatorAddress));
-      });
+  // const setProfiles = async (data: ValidatorAddressesQuery) => {
+  //   if (chainConfig.extra.profile) {
+  //     let profiles = [];
+  //     data?.validator?.filter((x) => x.validatorInfo).forEach((x) => {
+  //       const delegatorAddress = x.validatorInfo.selfDelegateAddress;
+  //       profiles.push(fetchDesmosProfile(delegatorAddress));
+  //     });
 
-      profiles = await Promise.allSettled(profiles);
+  //     profiles = await Promise.allSettled(profiles);
 
-      data?.validator?.filter((x) => x.validatorInfo).forEach((x, i) => {
-        const delegatorAddress = x.validatorInfo.selfDelegateAddress;
-        const [_, setProfile] = useRecoilState(writeProfile(delegatorAddress));
+  //     data?.validator?.filter((x) => x.validatorInfo).forEach((x, i) => {
+  //       const delegatorAddress = x.validatorInfo.selfDelegateAddress;
+  //       const [_, setProfile] = useRecoilState(writeProfile(delegatorAddress));
 
-        const profile = R.pathOr(undefined, [i, 'value'], profiles);
+  //       const profile = R.pathOr(undefined, [i, 'value'], profiles);
 
-        if (profile) {
-          // sets profile priority
-          const moniker = R.pathOr(undefined, ['nickname'], profile)
-          || R.pathOr('', ['validatorDescriptions', 0, 'moniker'], x);
-          const imageUrl = (
-            R.pathOr('', ['imageUrl'], profile)
-            || R.pathOr('', ['validatorDescriptions', 0, 'avatarUrl'], x)
-          );
+  //       if (profile) {
+  //         // sets profile priority
+  //         const moniker = R.pathOr(undefined, ['nickname'], profile)
+  //         || R.pathOr('', ['validatorDescriptions', 0, 'moniker'], x);
+  //         const imageUrl = (
+  //           R.pathOr('', ['imageUrl'], profile)
+  //           || R.pathOr('', ['validatorDescriptions', 0, 'avatarUrl'], x)
+  //         );
 
-          setProfile({
-            moniker,
-            imageUrl,
-          });
-        } else {
-          setProfile(false);
-        }
-      });
-    }
-  };
+  //         setProfile({
+  //           moniker,
+  //           imageUrl,
+  //         });
+  //       } else {
+  //         setProfile(false);
+  //       }
+  //     });
+  //   }
+  // };
 };
