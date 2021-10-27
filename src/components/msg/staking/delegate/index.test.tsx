3d904feeee8db12d5aa7ff16a80731a45471b04d
ryuash <ryuash123@gmail.com>
2021-10-27 14:39:38 +0800

    .

--- src/components/msg/staking/delegate/index.test.tsx
+++ src/components/msg/staking/delegate/index.test.tsx
@@ -7,15 +7,6 @@ import Delegate from '.';
 // ==================================
 // mocks
 // ==================================
-jest.mock('@contexts', () => ({
-  useChainContext: () => ({
-    findAddress: jest.fn(() => ({
-      moniker: 'moniker',
-      imageUrl: null,
-    })),
-  }),
-}));
-
 jest.mock('@components', () => ({
   Name: (props) => <div id="Name" {...props} />,
 }));
