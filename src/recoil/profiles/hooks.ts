9ecc55bc8e15868977577c82ffdcc7b4b262d70c
ryuash <ryuash123@gmail.com>
2021-10-27 11:07:57 +0800

    .

--- src/recoil/profiles/hooks.ts
+++ src/recoil/profiles/hooks.ts
@@ -58,64 +58,36 @@ export const useProfileRecoil = (address: string): AvatarName => {
  * Accepts a list of addresses and returns the appropriate profiles
  * @param address
  */
-export const useProfilesRecoil = (addresses: string[]): AvatarName => {
+export const useProfilesRecoil = (addresses: string[]): AvatarName[] => {
   const delegatorAddresses = useRecoilValue(readDelegatorAddresses(addresses));
   const rawProfiles: ProfileAtomState[] = useRecoilValue(readProfilesExist(addresses));
   const profiles = useRecoilValue(readProfiles(addresses));
-  // const [profiles, setProfiles] = useRecoilState(writeProfiles(delegatorAddresses)) as [AvatarName[], SetterOrUpdater<AvatarName[]>];
-
-  // const results: number[] = await Promise.all(arr.map(async (item): Promise<number> => {
-  //   await callAsynchronousOperation(item);
-  //   return item + 1;
-  // }));
 
   const fetchProfiles = useRecoilCallback(({ set }) => async () => {
-    const fetchedProfile = await getProfile(delegatorAddress);
-    if (fetchedProfile === null) {
-      set(writeProfile(delegatorAddress), null);
-    } else {
-      set(writeProfile(delegatorAddress), {
-        address: delegatorAddress,
-        name: fetchedProfile.nickname,
-        imageUrl: fetchedProfile.imageUrl,
-      });
-    }
-    // }
+    const fetchedProfiles = await Promise.all(rawProfiles.map(async (x, i) => {
+      const delegatorAddress = delegatorAddresses[i];
+      if (delegatorAddresses[i] && x === null) {
+        const fetchedProfile = await getProfile(delegatorAddresses[i]);
+        if (fetchedProfile === null) {
+          set(writeProfile(delegatorAddress), null);
+        } else {
+          set(writeProfile(delegatorAddress), {
+            address: delegatorAddress,
+            name: fetchedProfile.nickname,
+            imageUrl: fetchedProfile.imageUrl,
+          });
+        }
+      }
+    }));
+
+    return fetchedProfiles;
   });
 
   useEffect(() => {
-    const fetchProfile = async () => {
-      const fetchedProfiles = await Promise.all(rawProfiles.map(async (x, i) => {
-        if (delegatorAddresses[i] && x === null) {
-          const fetchedProfile = await getProfile(delegatorAddresses[i]);
-          if (fetchedProfile === null) {
-            set(writeProfile(delegatorAddress), null);
-          } else {
-            set(writeProfile(delegatorAddress), {
-              address: delegatorAddress,
-              name: fetchedProfile.nickname,
-              imageUrl: fetchedProfile.imageUrl,
-            });
-          }
-        }
-      }));
-
-      // const fetchedProfile = await getProfile(delegatorAddress);
-      // if (fetchedProfile === null) {
-      //   setProfile(null);
-      // } else {
-      //   setProfile({
-      //     address: delegatorAddress,
-      //     name: fetchedProfile.nickname,
-      //     imageUrl: fetchedProfile.imageUrl,
-      //   });
-      // }
-    };
-
     if (chainConfig.extra.profile) {
-      fetchProfile();
+      fetchProfiles();
     }
   }, []);
 
-  return profile;
+  return profiles;
 };
