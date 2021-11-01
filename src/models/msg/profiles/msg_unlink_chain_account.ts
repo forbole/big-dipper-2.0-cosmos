52474af77abf6cb26a728c30ff7ac1af1001e25d
ryuash <ryuash123@gmail.com>
2021-11-01 14:47:09 +0800

    add: models

---
+++ src/models/msg/profiles/msg_unlink_chain_account.ts
@@ -0,0 +1,32 @@
+import * as R from 'ramda';
+import { Categories } from '../types';
+
+class MsgUnlinkChainAccount {
+  public category: Categories;
+  public type: string;
+  public json: any;
+  public owner: string;
+  public chainName: string;
+  public target: string;
+
+  constructor(payload: any) {
+    this.category = 'profiles';
+    this.type = payload.type;
+    this.json = payload.json;
+    this.owner = payload.owner;
+    this.chainName = payload.chainName;
+    this.target = payload.target;
+  }
+
+  static fromJson(json: any) {
+    return new MsgUnlinkChainAccount({
+      json,
+      type: json['@type'],
+      owner: json.owner,
+      chainName: R.pathOr('', ['chain_name'], json),
+      target: json.target,
+    });
+  }
+}
+
+export default MsgUnlinkChainAccount;
