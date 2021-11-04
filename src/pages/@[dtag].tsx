9eaa1178e2912ed5a401c59dae03b3e6bfd82437
Ellie <isabelleya927@gmail.com>
2021-11-04 18:31:49 +0800

    update regex

---
+++ src/pages/@[dtag].tsx
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import ValidatorDetails from '@screens/validator_details';
+
+const ProfilePage = () => {
+  const router = useRouter();
+  console.log('router', router);
+
+  useEffect(() => {
+    const regex = /@\//;
+    const regexCheck = regex.exec(router.route);
+    if (regexCheck) {
+      router.replace('/');
+    }
+  }, []);
+
+  return (
+    <ValidatorDetails />
+  );
+};
+
+export default ProfilePage;
