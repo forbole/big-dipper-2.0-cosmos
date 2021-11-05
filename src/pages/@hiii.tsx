4b328371bbecab335b619b0b8e1cb057fd04760e
Ellie <isabelleya927@gmail.com>
2021-11-05 15:32:03 +0800

    src/screens/profile/index.tsx

--- src/pages/@hiii.tsx
+++ src/pages/@hiii.tsx
@@ -1,23 +1,8 @@
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import ProfileDetails from '@screens/profile';
 
 const ProfilePage = () => {
-  const router = useRouter();
-  console.log('router', router);
-
-  useEffect(() => {
-    const regex = /^\/@/;
-    const regexCheck = regex.test(router.route);
-    // const regexCheck = regex.test(router.query.dtag);
-    console.log(router);
-
-    if (!regexCheck) {
-      router.replace('/');
-    }
-  }, []);
-
   return (
-    <>hiii</>
+    <ProfileDetails />
   );
 };
 
---
+++ src/screens/profile/index.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+const ProfileDetails = () => {
+  const router = useRouter();
+  console.log('router', router);
+
+  useEffect(() => {
+    const regex = /^\/@/;
+    const regexCheck = regex.test(router.route);
+    // const regexCheck = regex.test(router.query.dtag);
+    console.log(router);
+
+    if (!regexCheck) {
+      router.replace('/');
+    }
+  }, []);
+
+  return (
+    <>good</>
+  );
+};
+
+export default ProfileDetails;
