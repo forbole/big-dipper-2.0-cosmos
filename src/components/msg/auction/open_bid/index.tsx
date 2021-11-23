29d1d7403ed24874da792044bbac9583c73784ee
ryuash <ryuash123@gmail.com>
2021-11-23 15:03:16 +0800

    add: changelog

--- src/components/msg/auction/open_bid/index.tsx
+++ src/components/msg/auction/open_bid/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import numeral from 'numeral';
 import Trans from 'next-translate/Trans';
 import { Typography } from '@material-ui/core';
-import { formatDenom } from '@utils/format_denom';
+import {
+  formatToken, formatNumber,
+} from '@utils/format_token';
 import { Name } from '@components';
 import { MsgOpenBid } from '@models';
 import { useProfileRecoil } from '@recoil/profiles';
@@ -12,8 +14,9 @@ const OpenBid = (props: {
 }) => {
   const { message } = props;
 
-  const amount = formatDenom(message.bidAmount.amount, message.bidAmount.denom);
-  const parsedAmount = `${numeral(amount.value).format(amount.format)} ${amount.denom.toUpperCase()}`;
+  const amount = formatToken(message.bidAmount.amount, message.bidAmount.denom);
+
+  const parsedAmount = `${formatNumber(amount.value, amount.exponent)} ${amount.displayDenom.toUpperCase()}`;
 
   const bidder = useProfileRecoil(message.bidder);
   const bidderMoniker = bidder ? bidder?.name : message.bidder;
