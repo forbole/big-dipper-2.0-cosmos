adc26886e7e9b90fad02f2c9a33f0db4fc829512
Ryuash <42913823+ryuash@users.noreply.github.com>
2021-11-26 18:07:15 +0800

    506/vp (#561)

--- src/screens/validators/components/list/components/voting_power/styles.ts
+++ src/screens/validators/components/list/components/voting_power/styles.ts
@@ -1,25 +1,37 @@
 import { makeStyles } from '@material-ui/core/styles';
 import Color from 'color';
 
-export const useStyles = (percentage: number) => {
+export const useStyles = (percentage: number, topVotingPower:boolean) => {
   const styles = makeStyles(
     (theme) => {
       return ({
         root: {
           '& .MuiTypography-body1': {
-            color: theme.palette.custom.fonts.fontTwo,
+            color: (
+              topVotingPower
+                ? theme.palette.custom.fonts.fontFour
+                : theme.palette.custom.fonts.fontTwo
+            ),
           },
         },
         chart: {
           display: 'flex',
           height: '2px',
           borderRadius: theme.shape.borderRadius,
-          background: Color(theme.palette.custom.primaryData.three).alpha(0.2).string(),
+          background: (
+            topVotingPower
+              ? Color(theme.palette.custom.fonts.fontFour).alpha(0.2).string()
+              : Color(theme.palette.custom.primaryData.three).alpha(0.2).string()
+          ),
           overflow: 'hidden',
         },
         active: {
           width: `${percentage}%`,
-          background: theme.palette.custom.primaryData.three,
+          background: (
+            topVotingPower
+              ? theme.palette.custom.fonts.fontFour
+              : theme.palette.custom.primaryData.three
+          ),
         },
         content: {
           display: 'flex',
@@ -27,7 +39,11 @@ export const useStyles = (percentage: number) => {
           justifyContent: 'space-between',
           marginBottom: theme.spacing(1),
           '& .percentage': {
-            color: theme.palette.custom.primaryData.three,
+            color: (
+              topVotingPower
+                ? theme.palette.custom.fonts.fontFour
+                : theme.palette.custom.primaryData.three
+            ),
           },
           [theme.breakpoints.up('lg')]: {
             marginBottom: 0,
