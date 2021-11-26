7e473e1c39c31f8c6e770e7eb10eca995ab10a04
Ellie <isabelleya927@gmail.com>
2021-11-26 17:24:17 +0800

    complete profile_details/connections/desktop test

---
+++ src/screens/profile_details/components/connections/components/desktop/index.test.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { MockTheme } from '@tests/utils';
+import Desktop from '.';
+
+// ==================================
+// mocks
+// ==================================
+const mockI18n = {
+  t: (key: string) => key,
+  lang: 'en',
+};
+jest.mock('next-translate/useTranslation', () => () => mockI18n);
+
+jest.mock('react-virtualized-auto-sizer', () => ({ children }: any) => children({
+  height: 600, width: 600,
+}));
+
+// ==================================
+// unit tests
+// ==================================
+describe('screen: ProfileDetails/Connections/Desktop', () => {
+  it('matches snapshot', () => {
+    const component = renderer.create(
+      <MockTheme>
+        <Desktop
+          items={[
+            {
+              network: 'native',
+              identifier: 'desmos1rzhewpmmdl72lhnxj6zmxr4v94f522s4ff2psv',
+              creationTime: '2021-08-31T17:02:28.575104',
+            },
+            {
+              network: 'emoney',
+              identifier: 'emoney1wke3ev9ja6rxsngld75r3vppcpet94xxnh63ry',
+              creationTime: '2021-08-31T17:02:28.575104',
+            }]}
+        />
+      </MockTheme>,
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+});
