1b8a1dce5ca4521f479a9ed2840cbc31a24f175e
yayay927 <32444273+yayay927@users.noreply.github.com>
2021-11-30 16:40:02 +0800

    554/ add @dtag to search bar (#568)

--- src/components/nav/components/seach_bar/index.tsx
+++ src/components/nav/components/seach_bar/index.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import useTranslation from 'next-translate/useTranslation';
 import { Search } from '@components';
+import { chainConfig } from '@src/configs';
 import { useSearchBar } from './hooks';
 
 const SearchBar: React.FC<{className?: string}> = ({ className }) => {
   const { t } = useTranslation('common');
-
   const {
     handleOnSubmit,
   } = useSearchBar(t);
 
+  let placeholderText;
+  if (chainConfig.extra.profile) {
+    placeholderText = t('searchBarPlaceholderDtag');
+  } else {
+    placeholderText = t('searchBarPlaceholder');
+  }
+
   return (
     <Search
       className={className}
-      placeholder={t('searchBarPlaceholder')}
+      placeholder={placeholderText}
       callback={handleOnSubmit}
     />
   );
