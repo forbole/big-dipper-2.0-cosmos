291423a914f8520062d5f840857d9442d1dcee5a
test <test>
2021-12-29 15:55:03 +0800

    update hooks test next/router

--- src/screens/profile_details/hooks.test.ts
+++ src/screens/profile_details/hooks.test.ts
@@ -5,18 +5,20 @@ import {
 } from '@testing-library/react-hooks';
 import { useProfileDetails } from './hooks';
 
-const mockPush = jest.fn();
-
 jest.mock('next/router', () => ({
   useRouter: () => ({
-    push: mockPush,
+    query: {
+      dtag: '@dtag',
+    },
+    replace: jest.fn(() => '/'),
+    push: jest.fn(() => ('/@dtag')),
   }),
 }));
 
 describe('hook: useProfileDetails', () => {
   test('correctly toggles profile open', async () => {
     const { result } = renderHook(() => useProfileDetails());
-    // initial
+    // initial state
     expect(result.current.state.loading).toBe(true);
     expect(result.current.state.exists).toBe(true);
     expect(result.current.state.desmosProfile).toBe(null);
@@ -29,6 +31,8 @@ describe('hook: useProfileDetails', () => {
     expect(result.current.state.exists).toBe(true);
     expect(result.current.state.desmosProfile).toBe(true);
 
+    // update url if renders profile UI but dtag and input is same in case insensitive
+
     // don't render profile UI if shouldShowProfile returns false
     act(() => {
       result.current.shouldShowProfile();
@@ -36,6 +40,14 @@ describe('hook: useProfileDetails', () => {
     expect(result.current.state.loading).toBe(true);
     expect(result.current.state.exists).toBe(false);
     expect(result.current.state.desmosProfile).toBe(true);
+  });
+
+  it('correctly update url', () => {
+    // return to homepage if chainConfig profile is false
+
+    // return to homepage if url dtag is not start with @
+
+    // look up profile data in graphql if profile is true and dtag search is start with @
   });
 });
 
