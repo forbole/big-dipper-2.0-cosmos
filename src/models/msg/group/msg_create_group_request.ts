ed36da529654a814e68989debd8c9c874683ce7a
test <test>
2022-01-06 15:43:27 +0800

    build msg_create_group_request.ts

--- src/models/msg/group/msg_create_group_request.ts
+++ src/models/msg/group/msg_create_group_request.ts
@@ -1,1 +1,25 @@
-export {};
+import { Categories } from '../types';
+
+class MsgCreateGroupRequest {
+  public category: Categories;
+  public type: string;
+  public json: any;
+  public admin: string;
+
+  constructor(payload: any) {
+    this.category = 'ecocredit';
+    this.json = payload.json;
+    this.type = payload.type;
+    this.admin = payload.admin;
+  }
+
+  static fromJson(json: any) {
+    return new MsgCreateGroupRequest({
+      json,
+      type: json['@type'],
+      admin: json.admin,
+    });
+  }
+}
+
+export default MsgCreateGroupRequest;
