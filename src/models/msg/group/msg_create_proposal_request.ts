e27c0a26ea098ff60858fcd19f6c05e56cf7e08b
test <test>
2022-01-06 23:46:11 +0800

    build msg_create_proposal_request.ts

--- src/models/msg/group/msg_create_proposal_request.ts
+++ src/models/msg/group/msg_create_proposal_request.ts
@@ -1,1 +1,25 @@
-export {};
+import { Categories } from '../types';
+
+class MsgCreateProposalRequest {
+  public category: Categories;
+  public type: string;
+  public json: any;
+  public address: string;
+
+  constructor(payload: any) {
+    this.category = 'ecocredit';
+    this.json = payload.json;
+    this.type = payload.type;
+    this.address = payload.address;
+  }
+
+  static fromJson(json: any) {
+    return new MsgCreateProposalRequest({
+      json,
+      type: json['@type'],
+      address: json.address,
+    });
+  }
+}
+
+export default MsgCreateProposalRequest;
