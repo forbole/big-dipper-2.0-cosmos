3177286eae1c4ebbfe2fb23b8ce4298ce8d34a27
test <test>
2022-01-07 14:40:20 +0800

    update msg_vote.ts

--- src/models/msg/group/msg_vote_request.ts
+++ src/models/msg/group/msg_vote_request.ts
@@ -1,1 +1,25 @@
-export {};
+import { Categories } from '../types';
+
+class MsgVoteRequest {
+  public category: Categories;
+  public type: string;
+  public json: any;
+  public voter: string;
+
+  constructor(payload: any) {
+    this.category = 'group';
+    this.json = payload.json;
+    this.type = payload.type;
+    this.voter = payload.voter;
+  }
+
+  static fromJson(json: any) {
+    return new MsgVoteRequest({
+      json,
+      type: json['@type'],
+      voter: json.voter,
+    });
+  }
+}
+
+export default MsgVoteRequest;
