9a41347488361241aa81632a42a32e2b924036ac
test <test>
2022-01-07 13:52:31 +0800

    build msg_update_group admin, members and metadata.ts

--- src/models/msg/group/msg_update_group_admin_request.ts
+++ src/models/msg/group/msg_update_group_admin_request.ts
@@ -1,1 +1,28 @@
-export {};
+import { Categories } from '../types';
+
+class MsgUpdateGroupAdminRequest {
+  public category: Categories;
+  public type: string;
+  public json: any;
+  public admin: string;
+  public newAdmin: string;
+
+  constructor(payload: any) {
+    this.category = 'group';
+    this.json = payload.json;
+    this.type = payload.type;
+    this.admin = payload.admin;
+    this.newAdmin = payload.newAdmin;
+  }
+
+  static fromJson(json: any) {
+    return new MsgUpdateGroupAdminRequest({
+      json,
+      type: json['@type'],
+      admin: json.admin,
+      newAdmin: json.newAdmin,
+    });
+  }
+}
+
+export default MsgUpdateGroupAdminRequest;
