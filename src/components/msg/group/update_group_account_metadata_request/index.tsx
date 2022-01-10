6e74810a0c2cdb1ac9bd5d3aa2d53a7aa7d98fff
test <test>
2022-01-10 12:18:47 +0800

    finish update msg/group module index.tsx files

---
+++ src/components/msg/group/update_group_account_metadata_request/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Trans from 'next-translate/Trans';
+import { Typography } from '@material-ui/core';
+import { Name } from '@components';
+import { MsgUpdateGroupAccountMetadataRequest } from '@models';
+import {
+  useProfileRecoil,
+} from '@recoil/profiles';
+
+const UpdateGroupAccountMetadataRequest = (props: {
+  message: MsgUpdateGroupAccountMetadataRequest;
+}) => {
+  const { message } = props;
+
+  const admin = useProfileRecoil(message.admin);
+  const adminMoniker = admin ? admin?.name : message.admin;
+
+  const address = useProfileRecoil(message.address);
+  const addressMoniker = address ? address?.name : message.address;
+
+  return (
+    <Typography>
+      <Trans
+        i18nKey="message_contents:MsgUpdateGroupAccountMetadataRequest"
+        components={[
+          (
+            <Name
+              address={message.admin}
+              name={adminMoniker}
+            />
+          ),
+          (
+            <Name
+              address={message.address}
+              name={addressMoniker}
+            />
+          ),
+        ]}
+      />
+    </Typography>
+  );
+};
+
+export default UpdateGroupAccountMetadataRequest;
