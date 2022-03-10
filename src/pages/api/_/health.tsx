64c0243a36ace62456b8807104dc5cbdc647c3c2
SpaghettiOverload <74422344+SpaghettiOverload@users.noreply.github.com>
2022-03-10 12:02:59 +0200

    Fixing refresh state

--- src/pages/api/_/health.tsx
+++ src/pages/api/_/health.tsx
@@ -1,19 +1,19 @@
-const blockHeight = fetch(process.env.NEXT_PUBLIC_GRAPHQL_URL, {
-  method: 'POST',
-  body: JSON.stringify({
-    query: `
-    query LatestBlockTimestamp {
-      block: block(order_by: {height: desc}, limit: 1) {
-          height
-      }
-    }
-    `,
-  }),
-})
-  .then((response) => response.json())
-  .then((r) => {
-    return r.data.block[0].height;
-  });
 export default async function handler(req, res) {
+  const blockHeight = fetch(process.env.NEXT_PUBLIC_GRAPHQL_URL, {
+    method: 'POST',
+    body: JSON.stringify({
+      query: `
+      query LatestBlockTimestamp {
+        block: block(order_by: {height: desc}, limit: 1) {
+            height
+        }
+      }
+      `,
+    }),
+  })
+    .then((response) => response.json())
+    .then((r) => {
+      return r.data.block[0].height;
+    });
   res.end(JSON.stringify(await blockHeight));
 }
