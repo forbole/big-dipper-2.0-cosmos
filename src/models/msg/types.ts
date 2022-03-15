f0cd3a412ee8f2fcdb8ef2d5550090d19dc45535
ryuash <ryuash123@gmail.com>
2022-03-15 13:56:50 +0800

    wip: need to do some research

--- src/models/msg/types.ts
+++ src/models/msg/types.ts
@@ -1,3 +1,3 @@
 export type BaseCategories = 'bank' | 'crisis' | 'distribution' | 'governance' | 'slashing' | 'staking' | 'profiles' | 'ibc' | 'ibc-transfer' | 'authz' | 'feegrant' | 'vesting' | 'others'
-export type CustomCategories = 'erc20'; // custom modules
+export type CustomCategories = 'erc20' | 'evm'; // custom modules
 export type Categories = BaseCategories | CustomCategories
