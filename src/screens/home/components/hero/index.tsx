e9c520a99dd21a82af6a8af9e463ec28b231363c
ryuash <ryuash123@gmail.com>
2022-03-22 12:12:09 +0800

    setup: child ovp

--- src/screens/home/components/hero/index.tsx
+++ src/screens/home/components/hero/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Box } from '@components';
+import { OnlineVotingPower } from './components';
 
 const Hero: React.FC<ComponentDefault> = (props) => {
   return (
-    <div>
-      hero
-    </div>
+    <Box className={props.className}>
+      <OnlineVotingPower />
+    </Box>
   );
 };
 
