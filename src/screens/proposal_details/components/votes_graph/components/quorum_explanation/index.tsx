d095a43b949ee4dc00e9503656bf333ae9faf405
yayay927 <isabelleya927@gmail.com>
2022-04-06 20:17:57 +0100

    update quorumExplanation index structure

--- src/screens/proposal_details/components/votes_graph/components/quorum_explanation/index.tsx
+++ src/screens/proposal_details/components/votes_graph/components/quorum_explanation/index.tsx
@@ -1,46 +1,21 @@
 import React from 'react';
-import classnames from 'classnames';
-import useTranslation from 'next-translate/useTranslation';
+import Trans from 'next-translate/Trans';
 import { Typography } from '@material-ui/core';
 import { useStyles } from './styles';
 
 const QuorumExplanation = () => {
-  const { t } = useTranslation('proposals');
   const classes = useStyles();
 
-  const conditions = [
-    {
-      display: '90% - 100%',
-      className: 'green',
-    },
-    {
-      display: '70% - 90%',
-      className: 'yellow',
-    },
-    {
-      display: '1% - 70%',
-      className: 'red',
-    },
-    {
-      display: '0%',
-      className: '',
-    },
-  ];
   return (
     <div className={classes.root}>
       <Typography>
-        {t('quorumExplanation')}
+        <Trans
+          i18nKey="proposals:quorumExplanation"
+          components={[
+            <b />,
+          ]}
+        />
       </Typography>
-      <div className={classes.itemWrapper}>
-        {conditions.map((x) => {
-          return (
-            <div className={classes.item} key={x.display}>
-              <Typography>{x.display}</Typography>
-              <div className={classnames(classes.condition, x.className)} />
-            </div>
-          );
-        })}
-      </div>
     </div>
   );
 };
