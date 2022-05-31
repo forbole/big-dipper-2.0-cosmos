0bc73a21d89591f9ebe7daa948a27367deb40d29
Wingman <ryuash123@gmail.com>
2022-05-31 09:09:11 -0400

    .

--- src/screens/app/utils.ts
+++ src/screens/app/utils.ts
@@ -3,7 +3,7 @@ export const OPEN_GRAPH_SEO = {
   site_name: 'Big Dipper',
   images: [
     {
-      url: 'https://bigdipper.live/images/big-dipper-social-media.png',
+      url: 'https://s3.bigdipper.live/osmosis.png',
       width: 800,
       height: 600,
       alt: 'Preview Photo',
