0d635aea273e7610a4cb1310dad913c5a34a41af
yayay927 <isabelleya927@gmail.com>
2022-08-16 17:16:29 +0800

    move liquid explanation folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -29,6 +29,7 @@ import LoadAndExist from './load_and_exist';
 import DesmosProfile from './desmos_profile';
 import TransactionListDetails from './transactions_list_details';
 import AvatarNameListMsg from './avatar_name_list_msg';
+import LiquidStakingExplanation from './liquid_staking_explanation';
 
 export {
   Layout,
@@ -62,4 +63,5 @@ export {
   DesmosProfile,
   TransactionListDetails,
   AvatarNameListMsg,
+  LiquidStakingExplanation,
 };
