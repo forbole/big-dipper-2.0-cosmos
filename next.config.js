f134a2115ebce6b818a19d980baf2df974bdb407
yayay927 <isabelleya927@gmail.com>
2022-08-29 22:59:07 +0800

    update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const nextTranslate = require('next-translate');
 
 module.exports = nextTranslate({
   poweredByHeader: false,
-  basePath: '/desmos',
+  basePath: '/agoric',
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
