76680bbf9ab1136c6c400f0dfc321719e4f1a480
teamchong <25894545+teamchong@users.noreply.github.com>
2022-09-19 23:02:27 +0800

    feat: BDU-549 Add Sentry.io issue tracking https://docs.sentry.io/platforms/javascript/guides/nextjs/

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+const { withSentryConfig } = require('@sentry/nextjs');
 const nextTranslate = require('next-translate');
 
-module.exports = nextTranslate({
+const moduleExports = nextTranslate({
   poweredByHeader: false,
   basePath: '/desmos',
   webpack: (config) => {
@@ -11,3 +16,33 @@ module.exports = nextTranslate({
     return config;
   },
 });
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+if (SENTRY_DSN) {
+  moduleExports.sentry = {
+    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
+    // for client-side builds. (This will be the default starting in
+    // `@sentry/nextjs` version 8.0.0.) See
+    // https://webpack.js.org/configuration/devtool/ and
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
+    // for more information.
+    hideSourceMaps: true,
+  };
+
+  const sentryWebpackPluginOptions = {
+    // Additional config options for the Sentry Webpack plugin. Keep in mind that
+    // the following options are set automatically, and overriding them is not
+    // recommended:
+    //   release, url, org, project, authToken, configFile, stripPrefix,
+    //   urlPrefix, include, ignore
+
+    silent: true, // Suppresses all logs
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options.
+  };
+
+  module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+} else {
+  module.exports = moduleExports;
+}
