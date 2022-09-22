45c7b53b1df37b10a91207e680efd9c7c5198a67
huichiaotsou <huichiao.tsou@gmail.com>
2022-09-22 15:35:36 +0800

    rm NODE_ENV=development from npm run dev

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -9,17 +9,20 @@ import generalConfig from './general_config.json';
  */
 const getChainConfig = () => {
   // chainType's & chainName's value will be passed in as npm argument
-  // const chainType = process.env.CHAIN_TYPE;
-  // const chainName = process.env.CHAIN_NAME;
+  const chainType = process.env.CHAIN_TYPE;
+  const chainName = process.env.CHAIN_NAME;
 
-  const chainType = 'mainnet';
-  const chainName = 'desmos';
+  // const chainType = 'mainnet';
+  // const chainName = 'desmos';
+
+  console.log("chainType, chainName: ", chainType, chainName);
+  
 
   switch (chainType) {
     case 'mainnet':
       return getMainnetChainConfig(chainName);
-    // case 'testnet':
-    //   return getTestnetChainConfig(chainName);
+    case 'testnet':
+      return getTestnetChainConfig(chainName);
     default:
       return chainConfigDesmosMainnet;
   }
