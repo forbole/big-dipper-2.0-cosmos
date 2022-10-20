6d85637fa111e3a9774e80b4f2ac051b01ed472d
huichiaotsou <huichiao.tsou@gmail.com>
2022-10-20 18:15:05 +0800

    set up comdex config

--- src/configs/testnet_configs.ts
+++ src/configs/testnet_configs.ts
@@ -1,9 +1,12 @@
+import chainConfigComdexTestnet from './chain_configs/comdex-testnet.json';
 import chainConfigCosmosTestnet from './chain_configs/cosmos-testnet.json';
 import chainConfigLikecoinTestnet from './chain_configs/likecoin-testnet.json';
 import baseChainConfig from './chain_configs/base-config.json';
 
 const getTestnetChainConfig = (chainName: string) => {
   switch (chainName) {
+    case 'comdex':
+      return chainConfigComdexTestnet;
     case 'cosmos':
       return chainConfigCosmosTestnet;
     case 'likecoin':
