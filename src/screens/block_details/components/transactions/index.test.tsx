073d6a4079e81964dbb3707517d52f9722c43daf
Magic Cat <pusz.monika@gmail.com>
2022-10-21 15:59:32 +0700

    updted screens/block_details txs unit tests

--- src/screens/block_details/components/transactions/index.test.tsx
+++ src/screens/block_details/components/transactions/index.test.tsx
@@ -7,6 +7,12 @@ import Transactions from '.';
 // ==================================
 // mocks
 // ==================================
+const mockI18n = {
+  t: (key: string) => key,
+  lang: 'en',
+};
+jest.mock('next-translate/useTranslation', () => () => mockI18n);
+
 jest.mock('@components', () => ({
   Box: (props) => <div id="Box" {...props} />,
   TransactionsList: (props) => <div id="TransactionsList" {...props} />,
@@ -27,6 +33,7 @@ describe('screen: BlockDetails/Transactions', () => {
                 hash: 'hash',
                 success: false,
                 timestamp: '',
+                type: [''],
                 messages: {
                   count: 3,
                   items: [],
@@ -37,6 +44,7 @@ describe('screen: BlockDetails/Transactions', () => {
                 hash: 'hash1',
                 success: true,
                 timestamp: '',
+                type: [''],
                 messages: {
                   count: 13,
                   items: [],
