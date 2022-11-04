9b983ede4e131afff7e026f473e60c6c7727486b
Magic Cat <pusz.monika@gmail.com>
2022-11-04 15:17:01 +0700

    updated msg ibc transfer unit tests

--- src/components/msg/ibc_transfer/transfer/index.test.tsx
+++ src/components/msg/ibc_transfer/transfer/index.test.tsx
@@ -18,7 +18,7 @@ jest.mock('@components', () => ({
 // ==================================
 describe('screen: TransactionDetails/IBCTransfer', () => {
   it('matches snapshot', () => {
-    const message = new MsgTransfer({
+    const message = MsgTransfer.fromJson({
       category: 'ibc',
       type: 'MsgTransfer',
       sender: 'desmos1jrld5g998gqm4yx26l6cvhxz7y5adgxquy94nz',
