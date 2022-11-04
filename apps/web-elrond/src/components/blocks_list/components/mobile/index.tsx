e5e26c6f3037ec3d512c2f173e67a106ecd345db
teamchong <25894545+teamchong@users.noreply.github.com>
2022-11-04 23:02:44 +0800

    fix: build error

--- apps/web-elrond/src/components/blocks_list/components/mobile/index.tsx
+++ apps/web-elrond/src/components/blocks_list/components/mobile/index.tsx
@@ -9,7 +9,7 @@ import {
   Divider, Typography,
 } from '@material-ui/core';
 import { BLOCK_DETAILS } from '@utils/go_to_page';
-import Single from '@components/single_block_mobile';
+import SingleBlockMobile from '@components/single_block_mobile';
 
 const Mobile: React.FC<{items: BlockType[]} & ComponentDefault> = (props) => {
   const { t } = useTranslation('blocks');
