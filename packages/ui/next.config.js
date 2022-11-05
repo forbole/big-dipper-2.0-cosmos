46d3068ad2af59e8d6949b39368323e2437b9b99
teamchong <25894545+teamchong@users.noreply.github.com>
2022-11-05 18:56:25 +0800

    revert: change ui from module to normal package

---
+++ packages/ui/next.config.js
@@ -0,0 +1,11 @@
+const nextTranslate = require('next-translate');
+
+// for turborepo
+const withTM = require('next-transpile-modules')(['ui']);
+const withSentry = require('shared-utils/configs/withSentry');
+const nextConfig = require('shared-utils/configs/next');
+
+// each chain has its own chains/<chainName>.json
+const config = require('shared-utils/configs/chains/base.json');
+
+module.exports = withTM(withSentry(nextTranslate(nextConfig(config))));
