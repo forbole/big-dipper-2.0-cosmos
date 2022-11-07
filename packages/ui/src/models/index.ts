aaf66f26f5c3234018818f6206bb1fc2bebf79ff
teamchong <25894545+teamchong@users.noreply.github.com>
2022-11-08 03:00:24 +0800

    feat: move big_dipper_network.ts to ui

--- packages/ui/src/models/index.ts
+++ packages/ui/src/models/index.ts
@@ -4,3 +4,8 @@ export { default as GovParams } from './gov_params';
 export { default as MintParams } from './mint_params';
 export { default as SlashingParams } from './slashing_params';
 export { default as StakingParams } from './staking_params';
+
+// ================================
+// Transaction Message Types
+// ================================
+export { default as MsgUnknown } from './msg/msg_unknown';
