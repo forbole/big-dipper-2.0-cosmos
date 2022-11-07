14e9cea406dc706d20e14482ea024d4a1575d661
Magic Cat <pusz.monika@gmail.com>
2022-11-07 17:53:37 +0700

    updated transacyon list details unit tests

--- src/components/transactions_list_details/index.test.tsx
+++ src/components/transactions_list_details/index.test.tsx
@@ -37,7 +37,7 @@ const txs = [{
 ];
 
 const props = {
-  hasNextPage: true,
+  hasNextPage: false,
   isNextPageLoading: false,
   loadNextPage: () => jest.fn(),
   loadMoreItems: () => jest.fn(),
@@ -46,19 +46,36 @@ const props = {
   transactions: txs,
 };
 
+const emptyProps = {
+  hasNextPage: true,
+  isNextPageLoading: false,
+  loadNextPage: () => jest.fn(),
+  loadMoreItems: () => jest.fn(),
+  isItemLoaded: () => true,
+  itemCount: 0,
+  transactions: [],
+};
+
 // ==================================
 // unit tests
 // ==================================
 describe('component: TransactionsListDetails', () => {
-  beforeEach(() => {
+  it('matches snapshot', () => {
     component = renderer.create(
       <MockTheme>
         <TransactionsListDetails {...props} />
       </MockTheme>,
     );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
 
-  it('it renders', () => {
+  it('handles empty data correctly', () => {
+    component = renderer.create(
+      <MockTheme>
+        <TransactionsListDetails {...emptyProps} />
+      </MockTheme>,
+    );
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
