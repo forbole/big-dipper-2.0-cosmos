85cbd7164f3ac66f8b8cb0c356374db66057b5c2
teamchong <25894545+teamchong@users.noreply.github.com>
2022-11-08 15:33:44 +0800

    chore: align packages for all worksapce

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,4 +1,7 @@
-export * as screens from './screens';
-export { default as generalConfig } from './generalConfig';
 export { default as chainConfig } from './chainConfig';
 export * as components from './components';
+export { default as generalConfig } from './generalConfig';
+export * as models from './models';
+export * as screens from './screens';
+export * as styles from './styles';
+export * as utils from './utils';
