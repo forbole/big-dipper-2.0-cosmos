53937b83beba9ce3eaa49d58db311d8b3288f357
Magic Cat <pusz.monika@gmail.com>
2022-11-08 16:57:24 +0700

    added inner app unit tests

---
+++ src/screens/app/components/inner_app/hooks.test.ts
@@ -0,0 +1,26 @@
+import {
+  renderHook,
+  cleanup,
+} from '@testing-library/react-hooks';
+import { useChainHealthCheck } from './hooks';
+
+const mockI18n = {
+  t: (key: string) => key,
+  lang: 'en',
+};
+jest.mock('next-translate/useTranslation', () => () => mockI18n);
+
+jest.mock('react-toastify', () => ({
+  toast: jest.fn(),
+}));
+
+describe('hook: useChainHealthCheck', () => {
+  test('handles open correctly', () => {
+    renderHook(() => useChainHealthCheck());
+  });
+});
+
+afterEach(() => {
+  cleanup();
+  jest.clearAllMocks();
+});
