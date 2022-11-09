94fb70f9a3b0581661411de4a546e40fda933c19
Magic Cat <pusz.monika@gmail.com>
2022-11-09 17:04:05 +0700

    add params page test

--- tests/params.spec.ts
+++ tests/params.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('params page', async ({ page }) => {
-    const url = await page.goto('https://cosmos.bigdipper.live/');
+    const url = await page.goto('https://desmos.bigdipper.live');
 
     await page.getByRole('link', { name: 'Params' }).click();
     await expect(page).toHaveURL(/.*params/);
 
-    await page.goto(`https://cosmos.bigdipper.live/params`);
+    await page.goto(`https://desmos.bigdipper.live/params`);
 
 });
