c7d66ad6a7694593462f13f9799838d8872060d1
Magic Cat <pusz.monika@gmail.com>
2022-11-11 12:17:30 +0700

    update validators page tests

--- e2e/validators.spec.ts
+++ e2e/validators.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('validators page', async ({ page }) => {
+test('validators list page', async ({ page }) => {
     await page.goto('https://desmos.bigdipper.live');
 
     await page.getByRole('link', { name: 'Validators' }).click();
@@ -10,10 +10,16 @@ test('validators page', async ({ page }) => {
     await page.getByRole('tab', { name: 'All Validators' }).click();
     await page.getByRole('tab', { name: 'Active' }).click();
 
+    await page.getByRole('button', { name: 'Validator' }).click();
+    await page.getByRole('button', { name: 'Voting Power' }).click();
+    await page.getByRole('button', { name: 'Commission' }).click();
+
     await page.getByPlaceholder('Search Validator').click();
     await page.getByPlaceholder('Search Validator').fill('Apollo');
     await page.getByPlaceholder('Search Validator').press('Enter');
     
     await page.goto(`https://desmos.bigdipper.live/validators/desmosvaloper17lca9smrdlwkznr92hypzrgsjkelnxeaacgrwq`);
+    await expect(page).toHaveURL(`https://desmos.bigdipper.live/validators/desmosvaloper134zrg6jn3a5l5jjpzv9eucdlw3nl2qelk0e992`);
+
 });
 
