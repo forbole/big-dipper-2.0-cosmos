18e1c0fdecfb45798b6a670e117ab4c68448b8df
Magic Cat <pusz.monika@gmail.com>
2022-11-11 16:16:36 +0700

    updated transactions page tests

--- e2e/transactions.spec.ts
+++ e2e/transactions.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 const deployURL = process.env.DEPLOY_URL ?? "http://localhost:3000";
+const transactionHash = '5922EA68378A74989E95BF477BF107A120CA1D006FDDA84BC93630BBF9A8E75B'
 
 test('transactions page', async ({ page }) => {
     // Test url
@@ -12,7 +13,7 @@ test('transactions page', async ({ page }) => {
     await expect(page).toHaveURL(/.*transactions/);
 
     // Test single transaction url
-    await page.goto(`${deployURL}/transactions/5922EA68378A74989E95BF477BF107A120CA1D006FDDA84BC93630BBF9A8E75B`);
-    await expect(page).toHaveURL(`${deployURL}/transactions/5922EA68378A74989E95BF477BF107A120CA1D006FDDA84BC93630BBF9A8E75B`);
+    await page.goto(`${deployURL}/transactions/${transactionHash}`);
+    await expect(page).toHaveURL(`${deployURL}/transactions/${transactionHash}`);
 });
 
