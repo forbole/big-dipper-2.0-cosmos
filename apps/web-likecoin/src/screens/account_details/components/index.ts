c79d78b4428fd4152f216ddcefb6e8bde340bdc7
teamchong <25894545+teamchong@users.noreply.github.com>
2022-11-12 15:13:05 +0800

    refactor: move balance into ui

--- apps/web-likecoin/src/screens/account_details/components/index.ts
+++ apps/web-likecoin/src/screens/account_details/components/index.ts
@@ -1,5 +1,5 @@
+import Balance from 'ui/screens/account_details/components/balance';
 import Overview from './overview';
-import Balance from './balance';
 import Staking from './staking';
 import Transactions from './transactions';
 import OtherTokens from './other_tokens';
