d5dfb3c65bde624d9920417edacb1ab16c3678b3
teamchong <25894545+teamchong@users.noreply.github.com>
2022-11-12 16:58:28 +0800

    refactor: move othertoken into ui

--- apps/web-assetmantle/src/screens/account_details/components/index.ts
+++ apps/web-assetmantle/src/screens/account_details/components/index.ts
@@ -1,7 +1,7 @@
 import Balance from 'ui/screens/account_details/components/balance';
+import OtherTokens from 'ui/screens/account_details/components/other_tokens';
 import Overview from './overview';
 import Staking from './staking';
 import Transactions from './transactions';
-import OtherTokens from './other_tokens';
 
 export { Overview, Balance, Staking, Transactions, OtherTokens };
