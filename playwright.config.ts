610b0442f623b7b2eaf9697cd207217a5f398162
teamchong <25894545+teamchong@users.noreply.github.com>
2022-11-25 02:34:47 +0800

    test: use non-headless mode in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,6 +37,7 @@ const config: PlaywrightTestConfig = {
   reporter: process.env.CI ? 'github' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    headless: !process.env.CI,
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
