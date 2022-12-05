17b533d8d0d47f46fcf15aff284e8ee9c764249d
teamchong <25894545+teamchong@users.noreply.github.com>
2022-12-05 17:15:02 +0800

    ci: update Dockerfile

--- playwright.config.ts
+++ playwright.config.ts
@@ -107,7 +107,7 @@ const config: PlaywrightTestConfig = {
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: `yarn workspace ${projectName} next start`,
+    command: process.env.PLAYWRIGHT_SERVER_COMMAND || `yarn workspace ${projectName} next start`,
     url: `http://localhost:${port}${basePath}`,
     ignoreHTTPSErrors: true,
     env: {
