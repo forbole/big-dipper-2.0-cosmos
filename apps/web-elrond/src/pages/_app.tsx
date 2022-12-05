22f2100267c8c44deb958c842ec2dd65bf2f8f29
teamchong <25894545+teamchong@users.noreply.github.com>
2022-12-05 11:01:45 +0800

    revert: vercel deployment

--- apps/web-elrond/src/pages/_app.tsx
+++ apps/web-elrond/src/pages/_app.tsx
@@ -1,7 +1,5 @@
 import App from '@/screens/app';
 import 'react-toastify/dist/ReactToastify.css';
 import 'shared-utils/assets/styles/global.css';
-import 'next/dist/server/node-polyfill-fetch';
-import 'next/dist/server/node-polyfill-web-streams';
 
 export default App;
