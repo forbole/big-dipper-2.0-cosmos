197e93a6c7674b4e72a2bc3216499be33d156917
teamchong <25894545+teamchong@users.noreply.github.com>
2022-12-05 10:50:11 +0800

    fix: vercel deployment

--- apps/web-sifchain/src/pages/_app.tsx
+++ apps/web-sifchain/src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import App from '@/screens/app';
 import 'react-toastify/dist/ReactToastify.css';
 import 'shared-utils/assets/styles/global.css';
+import 'next/dist/server/node-polyfill-fetch';
+import 'next/dist/server/node-polyfill-web-streams';
 
 export default App;
