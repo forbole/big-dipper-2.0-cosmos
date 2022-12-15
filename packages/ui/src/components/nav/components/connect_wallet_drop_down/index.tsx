36711ae35afb77cedb4f7685366fced7a0fb8eca
Magic Cat <pusz.monika@gmail.com>
2022-12-15 17:01:53 +0700

    wip: wallet dropdown menu

--- packages/ui/src/components/nav/components/connect_wallet_drop_down/index.tsx
+++ packages/ui/src/components/nav/components/connect_wallet_drop_down/index.tsx
@@ -1,22 +1,67 @@
 import Box from '@/components/box';
 import { useStyles } from '@/components/nav/components/connect_wallet_drop_down/styles';
 import classnames from 'classnames';
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
+import { useConnectWalletList } from '@/components/nav/components/connect_wallet/hooks';
+import useTranslation from 'next-translate/useTranslation';
+import Avatar from '@/components/avatar';
+import { getMiddleEllipsis } from '@/utils/get_middle_ellipsis';
+import CopyIcon from 'shared-utils/assets/icon-copy.svg';
+import { useAddress } from '@/utils/copy_to_clipboard';
+
 import React from 'react';
 
 const ConnectWalletDropDown: React.FC<{
   className?: string;
 }> = ({ className }) => {
+  const { t } = useTranslation('common');
   const classes = useStyles();
+  const { handleCopyToClipboard } = useAddress(t);
+  const address = 'desmos13yp2fq3tslq6mmtq4628q38xzj75ethzela9uu';
 
+  const { handleCancel } = useConnectWalletList();
   return (
     <Box className={classnames(className, classes.root)}>
-      <div
-        style={{
-          height: 600,
-        }}
-      >
-        {/* TO DO  */}
+      <div className={classes.walletDetails}>
+        <div className={classes.walletAvatar}>
+          <Avatar address={address} className={classes.avatar} />
+        </div>
+        <div className={classes.walletLabel}>
+          Wallet 3
+          <div>
+            <Typography variant="body2" component="a">
+              {getMiddleEllipsis(address, {
+                beginning: 9,
+                ending: 3,
+              })}
+            </Typography>
+            <CopyIcon onClick={() => handleCopyToClipboard(address)} className={classes.copyIcon} />
+          </div>
+        </div>
+        <Divider className={classes.divider} />
+        <div
+          onClick={handleCancel}
+          role="button"
+          className={classes.accountDetailsButton}
+          tabIndex={0}
+          aria-label="connect-wallet-button"
+        >
+          <div className={classes.accountDetailsLabel}>
+            <span className={classes.dot} /> {t('accountDetails')}
+          </div>
+        </div>
       </div>
+      <div
+        onClick={handleCancel}
+        role="button"
+        className={classes.changeWalletButton}
+        tabIndex={0}
+        aria-label="connect-wallet-button"
+      >
+        <div className={classes.changeWalletButtonLabel}>{t('changeWallet')}</div>
+      </div>
+      <Divider />
     </Box>
   );
 };
--- packages/ui/src/components/nav/components/connect_wallet_drop_down/styles.ts
+++ packages/ui/src/components/nav/components/connect_wallet_drop_down/styles.ts
@@ -2,13 +2,85 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const styles = makeStyles((theme) => ({
   root: {
-    background: theme.palette.background.paper,
-    width: '400px',
-    height: '600px',
+    background: '#5E5E5E',
+    width: '310px',
+    height: '310px',
     top: '80px',
-    left: '1330px',
+    left: '1365px',
     opacity: '20%',
-    // position: 'absolute',
+    borderRadius: '8px',
+  },
+  walletDetails: {
+    width: '281px',
+    height: '124px',
+    borderRadius: '8px',
+    backgroundColor: '#D9D9D9',
+    marginTop: theme.spacing(1),
+  },
+  changeWalletButton: {
+    width: '281px',
+    height: '46px',
+    borderRadius: '8px',
+    backgroundColor: '#4D4D4D',
+    marginTop: theme.spacing(3),
+    '&:hover': {
+      cursor: 'pointer',
+      background: '#5e5c5c',
+    },
+  },
+  changeWalletButtonLabel: {
+    padding: theme.spacing(1.5, 3),
+    fontSize: '16px',
+    fontWeight: 590,
+    lineHeight: '20px',
+  },
+  walletAvatar: {
+    padding: theme.spacing(2, 0, 0, 3),
+    display: 'inline-block',
+  },
+  walletLabel: {
+    padding: theme.spacing(2, 0, 0, 1),
+    display: 'inline-block',
+    fontSize: '14px',
+    fontWeight: 590,
+    lineHeight: '17px',
+    color: '#000000',
+  },
+  divider: {
+    margin: theme.spacing(1, 4.1, 1, 2),
+    color: '#34383E',
+  },
+  copyIcon: {
+    '&:hover': {
+      cursor: 'pointer',
+    },
+    width: '1rem',
+    marginLeft: theme.spacing(1),
+  },
+  avatar: {
+    height: '45px',
+    width: '45px',
+  },
+  accountDetailsButton: {
+    width: '232px',
+    height: '28px',
+    border: '1px solid #000000',
+    borderRadius: '1000px',
+    marginLeft: theme.spacing(3),
+  },
+  accountDetailsLabel: {
+    color: '#000000',
+    textAlign: 'center',
+    paddingTop: theme.spacing(0.5),
+  },
+  dot: {
+    height: theme.spacing(2),
+    width: theme.spacing(2),
+    backgroundColor: '#000000',
+    borderRadius: 50,
+    display: 'inline-flex',
+    marginTop: theme.spacing(0.5),
+    marginLeft: theme.spacing(1),
   },
 }));
 
