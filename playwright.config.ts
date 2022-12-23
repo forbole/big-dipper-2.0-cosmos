4e806a980d270efc9387e91db3d7c2ba0a88ad7a
teamchong <25894545+teamchong@users.noreply.github.com>
2022-12-23 13:01:47 +0800

    ci: adjust e2e webserver start up timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -77,10 +77,10 @@ const config: PlaywrightTestConfig = {
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry twice on CI, once on non-CI */
+  /* Retry twice on CI, none on non-CI */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -115,6 +115,7 @@ const config: PlaywrightTestConfig = {
       DEBUG: 'pw:webserver',
       RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED: 'false',
     },
+    timeout: process.env.CI ? 2 * 60 * 1000 : undefined,
     reuseExistingServer: !process.env.CI,
   },
 };
