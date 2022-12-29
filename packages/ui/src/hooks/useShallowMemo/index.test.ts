4803a4faddef234bf16612c69672c64af1d04c63
teamchong <25894545+teamchong@users.noreply.github.com>
2022-12-30 02:39:48 +0800

    test: update snapshots

--- packages/ui/src/hooks/useShallowMemo/index.test.ts
+++ packages/ui/src/hooks/useShallowMemo/index.test.ts
@@ -32,35 +32,39 @@ describe('hooks: useShallowMemo (with key)', () => {
   it('given the same props when render 10 times then return the same result', async () => {
     const initialProps = { value: ['a', 'b', 'c'], key: 'k1' };
     const initialProps2 = { value: ['a2', 'b2', 'c2'], key: 'k2' };
-    const { rerender, result } = renderHook((props) => useShallowMemo(props.key, props.value), {
+    const { rerender, result } = renderHook((props) => useShallowMemo(props.value, props.key), {
       initialProps,
     });
-    expect(result.current).toStrictEqual(initialProps);
+    expect(result.current).toStrictEqual(initialProps.value);
     for (let i = 0; i < 10; i += 1) {
       rerender(initialProps);
-      expect(result.current).toStrictEqual(initialProps);
+      expect(result.current).toStrictEqual(initialProps.value);
       rerender(initialProps2);
-      expect(result.current).toStrictEqual(initialProps);
+      expect(result.current).toStrictEqual(initialProps2.value);
     }
   });
   it('given diff props when rerender then return a new result', async () => {
     const initialProps = { value: ['a', 'b', 'c'], key: 'k1' };
     const initialProps2 = { value: ['a2', 'b2', 'c2'], key: 'k2' };
-    const { rerender, result } = renderHook((props) => useShallowMemo(props), { initialProps });
+    const { rerender, result } = renderHook((props) => useShallowMemo(props.value, props.key), {
+      initialProps,
+    });
     rerender({ value: ['d', 'e', 'f'], key: 'k1' });
-    expect(result.current).not.toEqual(initialProps);
+    expect(result.current).not.toEqual(initialProps.value);
     rerender({ value: ['d2', 'e2', 'f2'], key: 'k2' });
-    expect(result.current).not.toEqual(initialProps2);
+    expect(result.current).not.toEqual(initialProps2.value);
   });
   it('given new props with same values when rerender then return the same result', async () => {
     const initialProps = { value: ['a', 'b', 'c'], key: 'k1' };
     const initialProps2 = { value: ['a2', 'b2', 'c2'], key: 'k2' };
-    const { rerender, result } = renderHook((props) => useShallowMemo(props), { initialProps });
+    const { rerender, result } = renderHook((props) => useShallowMemo(props.value, props.key), {
+      initialProps,
+    });
     rerender({ value: ['a', 'b', 'c'], key: 'k1' });
-    expect(result.current).toStrictEqual(initialProps);
+    expect(result.current).toStrictEqual(initialProps.value);
     expect(result.current).toEqual(['a', 'b', 'c']);
     rerender({ value: ['a2', 'b2', 'c2'], key: 'k2' });
-    expect(result.current).toStrictEqual(initialProps2);
-    expect(result.current).toEqual(['a', 'b', 'c']);
+    expect(result.current).toStrictEqual(initialProps2.value);
+    expect(result.current).toEqual(['a2', 'b2', 'c2']);
   });
 });
