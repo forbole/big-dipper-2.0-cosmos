e6aa5ba7b1209352842f11b4731d3bd764811eb5
Magic Cat <pusz.monika@gmail.com>
2022-12-30 11:29:47 +0700

    add authorize connection dialog

---
+++ packages/ui/src/components/nav/components/connect_wallet/authorize_connection/index.tsx
@@ -0,0 +1,68 @@
+import { useConnectWalletList } from '@/components/nav/components/connect_wallet/hooks';
+import { useStyles } from '@/components/nav/components/connect_wallet/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
+import useTranslation from 'next-translate/useTranslation';
+import React from 'react';
+import Loading from '@/components/loading';
+
+const AuthorizeConnectionDialog: React.FC = () => {
+  const classes = useStyles();
+  const { t } = useTranslation('common');
+  const {
+    handleConnectWallet,
+    handleCloseAuthorizeConnectionDialog,
+    openAuthorizeConnectionDialog,
+  } = useConnectWalletList();
+
+  return (
+    <div>
+      <Dialog
+        maxWidth="md"
+        onClose={handleCloseAuthorizeConnectionDialog}
+        open={openAuthorizeConnectionDialog}
+        className={classes.dialog}
+      >
+        <DialogTitle disableTypography>
+          <div className={classes.warningMsg}>
+            <div>
+              <Typography variant="h2" align="center" className={classes.msgHeader}>
+                {t('waitingForAuthorize')}
+              </Typography>
+            </div>
+            <IconButton
+              aria-label="close"
+              onClick={handleCloseAuthorizeConnectionDialog}
+              className={classes.closeButton}
+            >
+              <CloseIcon />
+            </IconButton>
+          </div>
+        </DialogTitle>
+        <DialogContent>
+          <div className={classes.msgContent}>
+            <Typography variant="h4" align="center">
+              {t('pleasePairWallet')}
+            </Typography>
+          </div>
+          <Loading className={classes.loading} />
+        </DialogContent>
+        <DialogActions>
+          <div className={classes.actions}>
+            <Button onClick={handleConnectWallet} color="primary" className={classes.actionsButton}>
+              <Typography variant="h3">{t('Continue')}</Typography>
+            </Button>
+          </div>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default AuthorizeConnectionDialog;
