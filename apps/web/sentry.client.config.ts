4ce79a4743d761570f55aeb29fbf6aa977e60894
teamchong <25894545+teamchong@users.noreply.github.com>
2023-01-02 21:44:30 +0800

    refactor: remove unused

--- apps/web/sentry.client.config.ts
+++ apps/web/sentry.client.config.ts
@@ -1,7 +1,5 @@
-import chainConfig from '@/chainConfig';
 import * as Sentry from '@sentry/nextjs';
 
-const { chainName } = chainConfig();
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   release: process.env.NEXT_PUBLIC_RELEASE,
