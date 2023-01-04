9d4da4ba5edbb34aa0ed9cde46d73f1f4511247c
teamchong <25894545+teamchong@users.noreply.github.com>
2023-01-04 15:20:39 +0800

    feat: Update withStyles import

--- packages/ui/src/components/markdown/index.tsx
+++ packages/ui/src/components/markdown/index.tsx
@@ -1,6 +1,6 @@
 import DOMPurify from 'dompurify';
 import ReactMarkdown from 'markdown-to-jsx';
-// import { withStyles } from '@mui/material/styles';
+// import { withStyles } from '@mui/styles';
 import Typography from '@mui/material/Typography';
 // import Link from '@mui/material/Link';
 
