78b389bbeed5946a01ce27a503d28065b6df0de7
Magic Cat <pusz.monika@gmail.com>
2023-01-05 17:51:36 +0700

    rm recoil/wallet

--- packages/ui/src/recoil/wallet/index.ts
+++
@@ -1,9 +0,0 @@
-export { atomState } from '@/recoil/wallet/atom';
-export { useWalletRecoil } from '@/recoil/wallet/hooks';
-export {
-  readUserAddress,
-  writeUserAddress,
-  readIsUserLoggedIn,
-  writeIsUserLoggedIn,
-} from '@/recoil/wallet/selectors';
-export type { AtomState } from '@/recoil/wallet/types';
