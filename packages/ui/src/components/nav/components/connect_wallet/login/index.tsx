4daf6bc5289e87baa0620f510e062ab5bce35920
Magic Cat <pusz.monika@gmail.com>
2023-01-06 13:28:10 +0700

    mv wallet dialog to login dialog component

---
+++ packages/ui/src/components/nav/components/connect_wallet/login/index.tsx
@@ -0,0 +1,86 @@
+import { useStyles } from '@/components/nav/components/connect_wallet/styles';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
+import useTranslation from 'next-translate/useTranslation';
+import React from 'react';
+
+const LoginDialog: React.FC<{
+  open: boolean;
+  onClose: () => void;
+  onContinue: () => void;
+  setWallet: (walletOption: string) => void;
+}> = (props) => {
+  const classes = useStyles();
+  const { t } = useTranslation('common');
+
+  return (
+    <div>
+      <Dialog maxWidth="md" onClose={props.onClose} open={props.open} className={classes.dialog}>
+        <DialogTitle disableTypography>
+          <div className={classes.header}>
+            <div className={classes.title}>
+              <Typography variant="h2" gutterBottom>
+                {t('logIn')}
+              </Typography>
+            </div>
+            <div>
+              <Typography variant="h4">{t('welcomeToBigDipper')}</Typography>
+            </div>
+            <IconButton aria-label="close" onClick={props.onClose} className={classes.closeButton}>
+              <CloseIcon />
+            </IconButton>
+          </div>
+        </DialogTitle>
+        <DialogContent style={{ overflow: 'visible' }}>
+          <div className={classes.dialogContent}>
+            <div className={classes.connectWalletButton}>
+              <Button
+                onClick={() => props.setWallet('Wallet Connect')}
+                color="default"
+                aria-label="connect-walletconnect-wallet-button"
+                className={classes.walletButton}
+              >
+                <div className={classes.walletConnectLabel}>Wallet connect</div>
+              </Button>
+            </div>
+            <div className={classes.connectKeplrButton}>
+              <Button
+                onClick={() => props.setWallet('Keplr Wallet')}
+                color="default"
+                aria-label="connect-keplr-wallet-button"
+                className={classes.walletButton}
+              >
+                <div className={classes.label}>Keplr</div>
+              </Button>
+            </div>
+            <div className={classes.connectButterButton}>
+              <Button
+                onClick={() => props.setWallet('Butter')}
+                color="default"
+                aria-label="connect-butter-wallet-button"
+                className={classes.walletButton}
+              >
+                <div className={classes.label}>Butter</div>
+              </Button>
+            </div>
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <div className={classes.actions}>
+            <Button onClick={props.onContinue} color="primary" className={classes.actionsButton}>
+              <Typography variant="h3">{t('continue')}</Typography>
+            </Button>
+          </div>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export default LoginDialog;
