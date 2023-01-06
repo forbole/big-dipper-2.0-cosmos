8a1c7dc774b6db818f857f039b8ac9a455c60d94
teamchong <25894545+teamchong@users.noreply.github.com>
2023-01-06 13:05:46 +0800

    docs: add comments on vercel-deploy.js

--- vercel-deploy.js
+++ vercel-deploy.js
@@ -1,14 +1,27 @@
 /* eslint-disable @typescript-eslint/no-var-requires, no-console, turbo/no-undeclared-env-vars */
+
+/**
+ * This script is used to build the correct project based on the PR.
+ * the title suffix should be in the format of [workspace-name]
+ * otherwise, it will build the web project
+ */
 const { execSync } = require('child_process');
 
 console.log('running vercel-deploy.js');
 
+// VERCEL_GIT_PULL_REQUEST_ID is the pull request id
 const pullId = process.env.VERCEL_GIT_PULL_REQUEST_ID;
-const apiToken = process.env.GITHUB_API_TOKEN;
-
 if (!pullId) throw new Error('VERCEL_GIT_PULL_REQUEST_ID is not defined');
+
+// GITHUB_API_TOKEN is the github api token, we need it to get the pull request title
+const apiToken = process.env.GITHUB_API_TOKEN;
 if (!apiToken) throw new Error('GITHUB_API_TOKEN is not defined');
 
+/**
+ * It executes a shell command and returns the result
+ * @param command - The command to execute.
+ * @returns The result of the command being executed.
+ */
 function execShell(command) {
   console.log(`executing command`);
   const result = execSync(command, { env: process.env }).toString();
@@ -16,6 +29,7 @@ function execShell(command) {
   return result;
 }
 
+/* Getting the pull request title. */
 const response = execShell(
   `curl ` +
     `-H 'Accept: application/vnd.github+json' ` +
@@ -23,9 +37,12 @@ const response = execShell(
     `-H 'X-GitHub-Api-Version: 2022-11-28' ` +
     `https://api.github.com/repos/forbole/big-dipper-2.0-cosmos/pulls/${pullId}`
 );
-
 const { title } = JSON.parse(response);
 
+/**
+ * Getting the list of projects in the workspace and then
+ * finding the project that matches the title of the PR.
+ */
 const projects = execShell(`yarn workspaces list --json`);
 
 const projectList = projects
@@ -36,8 +53,10 @@ const projectList = projects
 
 const project = projectList.find((p) => title.endsWith(`[${p}]`)) || 'web';
 
+/* Building the project. */
 execShell(`yarn workspace ${project} next build`);
 
+/* Moving the built project to the web folder. */
 if (project !== 'web') {
   execShell(`rm -rf apps/web, mv apps/${project} apps/web`);
 }
