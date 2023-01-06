7266083c8748570a8dc2b855b6f00e1f7a4cb17f
teamchong <25894545+teamchong@users.noreply.github.com>
2023-01-06 14:15:18 +0800

    fix: move the output

--- vercel-deploy.js
+++ vercel-deploy.js
@@ -58,3 +58,8 @@ const project = projectList.find((p) => title.endsWith(`[${p}]`)) || 'web';
 execShell(
   `BASE_PATH=/ DIST_DIR=${join(__dirname, 'apps/web/.next')} yarn workspace ${project} next build`
 );
+
+/* Move the built project to the web folder. */
+if (project !== 'web') {
+  execShell(`rm -rf apps/web && mv apps/${project} apps/web`);
+}
