bf9c85adc52ac41a3ee79b5560761b5b44958bec
Magic Cat <pusz.monika@gmail.com>
2023-01-10 16:50:50 +0700

    updated user recoil atom.ts

--- packages/ui/src/recoil/user/atom.ts
+++ packages/ui/src/recoil/user/atom.ts
@@ -1,28 +1,7 @@
 import { atom } from 'recoil';
 
-export const PubKeyType = {
-  Secp256k1: 'tendermint/PubKeySecp256k1' as const,
-  Ed25519: 'tendermint/PubKeyEd25519' as const,
-  Empty: '' as const,
-};
-
 export interface PubKey {
-  readonly type: typeof PubKeyType.Secp256k1 | typeof PubKeyType.Ed25519 | typeof PubKeyType.Empty;
-  readonly value: string;
-}
-
-export interface Secp256k1PubKey {
-  readonly type: 'tendermint/PubKeySecp256k1';
-  readonly value: string;
-}
-
-export interface Ed25519PubKey {
-  readonly type: 'tendermint/PubKeyEd25519';
-  readonly value: string;
-}
-
-export interface EmptyPubKey {
-  readonly type: '';
+  readonly type: string;
   readonly value: string;
 }
 
