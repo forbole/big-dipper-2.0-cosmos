3342fe7b27263b7e4cd2584a14cf0d0ef07e9fc3
Magic Cat <pusz.monika@gmail.com>
2023-01-10 14:42:30 +0700

    add clickawaylistener to wallet details

--- packages/ui/src/components/nav/components/wallet_details/index.tsx
+++ packages/ui/src/components/nav/components/wallet_details/index.tsx
@@ -6,32 +6,41 @@ import Avatar from '@/components/avatar';
 import WalletDropDown from '@/components/nav/components/wallet_drop_down';
 import { readUserAddress, readIsUserLoggedIn } from '@/recoil/user';
 import { useRecoilValue } from 'recoil';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 
 const WalletDetails: React.FC<{
   className?: string;
 }> = (props) => {
   const classes = useStyles();
-  const { showWalletDetails, handleShowWalletDetails } = useConnectWalletList();
+  const { showWalletDetails, handleShowWalletDetails, handleCloseWalletDetails } =
+    useConnectWalletList();
   const loggedIn = useRecoilValue(readIsUserLoggedIn);
   const userAddress = useRecoilValue(readUserAddress);
 
   return (
-    <div>
-      <div
-        onClick={handleShowWalletDetails}
-        role="button"
-        className={classnames(props.className)}
-        tabIndex={0}
-        aria-label="wallet-details-button"
-      >
-        {loggedIn ? <Avatar address={userAddress ?? ''} className={classes.avatar} /> : null}
+    <ClickAwayListener
+      onClickAway={(e) => {
+        e.preventDefault();
+        handleCloseWalletDetails();
+      }}
+    >
+      <div>
+        <div
+          onClick={handleShowWalletDetails}
+          role="button"
+          className={classnames(props.className)}
+          tabIndex={0}
+          aria-label="wallet-details-button"
+        >
+          {loggedIn ? <Avatar address={userAddress ?? ''} className={classes.avatar} /> : null}
+        </div>
+        <WalletDropDown
+          className={classnames(classes.networkList, {
+            open: showWalletDetails,
+          })}
+        />
       </div>
-      <WalletDropDown
-        className={classnames(classes.networkList, {
-          open: showWalletDetails,
-        })}
-      />
-    </div>
+    </ClickAwayListener>
   );
 };
 
