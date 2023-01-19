0bd92447efd217eeaa561f0fdf70827f54f3a132
teamchong <25894545+teamchong@users.noreply.github.com>
2023-01-19 21:02:46 +0800

    style: change access control format

--- apps/web-provenance/src/screens/assets/components/AccessControl/index.tsx
+++ apps/web-provenance/src/screens/assets/components/AccessControl/index.tsx
@@ -7,8 +7,9 @@ import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 import Typography from '@mui/material/Typography';
-import { FC } from 'react';
+import { FC, Fragment } from 'react';
 
 const AccessControl: FC<{ accessControls: AccessControl[] }> = ({ accessControls }) => {
   const { classes } = useStyles();
@@ -17,27 +18,41 @@ const AccessControl: FC<{ accessControls: AccessControl[] }> = ({ accessControls
       <Typography className={classes.label} variant="h4">
         Access Control
       </Typography>
-      {accessControls?.map(({ address, permissions }) => (
-        <>
-          <AvatarName address={address} name={address} />
-          <Table style={{ maxWidth: 600 }}>
-            <TableHead>
+      {!!accessControls?.length && (
+        <Table style={{ maxWidth: 600 }}>
+          <TableHead>
+            <TableRow>
               {permissionList.map(([permission, description]) => (
                 <TableCell key={permission} title={description} aria-label={description}>
                   <span>{permission}</span>
                 </TableCell>
               ))}
-            </TableHead>
-            <TableBody>
-              {permissionList.map(([permission, description], i) => (
-                <TableCell key={permission} title={description} aria-label={description}>
-                  <Checkbox disabled checked={permissions.some((p) => p === i)} key={permission} />
-                </TableCell>
-              ))}
-            </TableBody>
-          </Table>
-        </>
-      ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {accessControls?.map(({ address, permissions }) => (
+              <Fragment key={address}>
+                <TableRow>
+                  <TableCell colSpan={permissionList.length}>
+                    <AvatarName address={address} name={address} />
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  {permissionList.map(([permission, description], i) => (
+                    <TableCell key={permission} title={description} aria-label={description}>
+                      <Checkbox
+                        disabled
+                        checked={permissions.some((p) => p === i)}
+                        key={permission}
+                      />
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </Fragment>
+            ))}
+          </TableBody>
+        </Table>
+      )}
     </Box>
   );
 };
