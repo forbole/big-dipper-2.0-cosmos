49c38e42e7633d47f47c73a499242706f6dc79f3
teamchong <25894545+teamchong@users.noreply.github.com>
2023-02-01 08:19:42 +0800

    feat: remove sifchain

--- apps/web-sifchain/src/models/msg/types.ts
+++
@@ -1,5 +0,0 @@
-import type { BaseCategories, Log } from 'ui/models/msg/types';
-
-export type CustomCategories = 'clp' | 'dispensation' | 'ethbridge' | 'tokenregistry'; // custom modules
-export type Categories = BaseCategories | CustomCategories;
-export type { Log };
