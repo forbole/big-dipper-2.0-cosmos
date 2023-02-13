b6b40f7195e64ecedbddc19212220b57274de6d6
teamchong <25894545+teamchong@users.noreply.github.com>
2023-02-13 15:05:34 +0800

    test: fix wasm-pack test

--- packages/rust-wasm/build.js
+++ packages/rust-wasm/build.js
@@ -2,21 +2,18 @@
 const { execSync } = require('child_process');
 const { resolve } = require('path');
 
-const [_node, _buildjs, arg, ...argRest] = process.argv;
+const [_node, _buildjs, ...argRest] = process.argv;
 
-if (arg === 'test') {
-  execSync(`wasm-pack test --node ${argRest.join(' ')}`, {
+if (argRest[0] === 'test') {
+  execSync(`wasm-pack test --node`, {
     cwd: resolve(__dirname, '../..'),
   });
-} else if (arg === 'build') {
-  execSync(
-    `wasm-pack build --target web --out-dir ${resolve(__dirname, 'pkg')} ${argRest.join(' ')}`,
-    {
-      cwd: resolve(__dirname, '../..'),
-    }
-  );
+} else if (argRest[0] === 'build') {
+  execSync(`wasm-pack build --target web --out-dir ${resolve(__dirname, 'pkg')}}`, {
+    cwd: resolve(__dirname, '../..'),
+  });
 } else {
-  execSync(`wasm-pack ${[arg, ...argRest].join(' ')}`, {
+  execSync(`wasm-pack ${argRest.join(' ')}`, {
     cwd: resolve(__dirname, '../..'),
   });
 }
