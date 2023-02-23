2c2a8789fa6884cff768f078038ca154d1ac10df
rachelhox <rachelhoyuenyi@gmail.com>
2023-02-23 13:22:59 +0800

    web-desmos: add zhs zht

--- apps/web-desmos/next-i18next.config.js
+++ apps/web-desmos/next-i18next.config.js
@@ -4,7 +4,7 @@ const { resolve } = require('path');
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en'],
+    locales: ['en', 'zht', 'zhs'],
   },
   localePath: resolve('./public/locales'),
 };
