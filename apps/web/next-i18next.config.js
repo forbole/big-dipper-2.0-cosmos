0f30ba784d5f2243d93e9c97ca6084b21d8f21af
rachelhox <rachelhoyuenyi@gmail.com>
2023-03-12 19:07:09 +0800

    web: locale settings

--- apps/web/next-i18next.config.js
+++ apps/web/next-i18next.config.js
@@ -4,7 +4,7 @@ const { resolve } = require('path');
 module.exports = {
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'zht'],
+    locales: ['en', 'zht', 'zhs', 'it', 'pl'],
   },
   localePath: resolve('./public/locales'),
 };
