1486b6900254c11730618c99d5cafa5ee915211e
rachelhox <rachelhoyuenyi@gmail.com>
2023-03-27 11:48:43 +0800

    nav bar revamp

--- packages/ui/src/components/nav/components/desktop/components/action_bar/components/network_list/index.tsx
+++ packages/ui/src/components/nav/components/desktop/components/action_bar/components/network_list/index.tsx
@@ -1,11 +1,9 @@
 import { FC } from 'react';
-import { useRecoilValue } from 'recoil';
-import BigDipperLogoRed from 'shared-utils/assets/big-dipper-red.svg';
-import BigDipperLogoWhite from 'shared-utils/assets/big-dipper-white.svg';
+import { useTranslation } from 'next-i18next';
 import Box from '@/components/box';
+import Typography from '@mui/material/Typography';
 import useStyles from '@/components/nav/components/desktop/components/action_bar/components/network_list/styles';
 import Networks from '@/components/nav/components/networks';
-import { readTheme } from '@/recoil/settings';
 
 type NetworkListProps = {
   className?: string;
@@ -14,7 +12,7 @@ type NetworkListProps = {
 
 const NetworkList: FC<NetworkListProps> = ({ className, actionHeight }) => {
   const { classes, cx } = useStyles();
-  const theme = useRecoilValue(readTheme);
+  const { t } = useTranslation('common');
 
   return (
     <Box className={cx(classes.root, className)}>
@@ -23,7 +21,7 @@ const NetworkList: FC<NetworkListProps> = ({ className, actionHeight }) => {
           height: actionHeight,
         }}
       >
-        {theme === 'light' ? <BigDipperLogoRed /> : <BigDipperLogoWhite />}
+        <Typography variant="h2">{t('select a chain')}</Typography>
       </div>
       <Networks className={classes.content} />
     </Box>
