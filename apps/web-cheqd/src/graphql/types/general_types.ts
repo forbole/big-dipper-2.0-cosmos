9d998bc878761d21e9c24caa5e59445e841e5d90
teamchong <25894545+teamchong@users.noreply.github.com>
2023-04-11 09:55:16 +0000

    feat: yarn graphql:codegen

--- apps/web-cheqd/src/graphql/types/general_types.ts
+++ apps/web-cheqd/src/graphql/types/general_types.ts
@@ -8130,7 +8130,7 @@ export type TokenomicsLazyQueryHookResult = ReturnType<typeof useTokenomicsLazyQ
 export type TokenomicsQueryResult = Apollo.QueryResult<TokenomicsQuery, TokenomicsQueryVariables>;
 export const TopAccountsDocument = gql`
     query TopAccounts($limit: Int = 100, $offset: Int = 0) {
-  top_accounts(limit: $limit, offset: $offset) {
+  top_accounts(limit: $limit, offset: $offset, order_by: {sum: desc}) {
     address
     available
     delegation
