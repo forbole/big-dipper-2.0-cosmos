418af9c1f803f7ff5f547f95a582091b51e3e590
Magic Cat <pusz.monika@gmail.com>
2023-05-15 16:46:25 +0700

    updated stake button

--- packages/ui/src/screens/validators/components/list/components/staking/index.tsx
+++ packages/ui/src/screens/validators/components/list/components/staking/index.tsx
@@ -23,55 +23,63 @@ const StakeButton = (props: StakeButtonProps) => {
   const { classes } = useStyles();
   const { t } = useTranslation();
   const {
-    handleClick,
-    handleClose,
-    handleDelegate,
-    handleRedelegate,
-    handleUndelegate,
-    open,
     anchorEl,
-    openStakingDialog,
+    openStakingMenu,
+    openDelegateDialog,
     openRedelegateDialog,
     openUndelegateDialog,
+    handleOpenStakingMenu,
+    handleOpenDelegateDialog,
+    handleOpenRedelegateDialog,
+    handleOpenUndelegateDialog,
+    handleCloseStakingMenu,
+    handleCloseDelegateDialog,
+    handleCloseRedelegateDialog,
+    handleCloseUndelegateDialog,
   } = useStakingHooks();
 
   return (
     <div>
-      <Button id="stake-button" onClick={handleClick} className={classes.staking}>
+      <Button id="stake-button" onClick={handleOpenStakingMenu} className={classes.staking}>
         <Typography variant="body2" className={classes.label}>
           {t('validators:stake')}
         </Typography>
         <ExpandMoreIcon />
       </Button>
-      <Menu id="stake-menu" anchorEl={anchorEl} open={open} onClose={handleClose}>
-        <MenuItem onClick={handleDelegate}> {t('validators:delegate')}</MenuItem>
+      <Menu
+        id="stake-menu"
+        anchorEl={anchorEl}
+        open={openStakingMenu}
+        onClose={handleCloseStakingMenu}
+      >
+        <MenuItem onClick={handleOpenDelegateDialog}> {t('validators:delegate')}</MenuItem>
         <DelegateDialog
-          open={openStakingDialog}
-          onClose={handleClose}
-          address={props.address}
+          open={openDelegateDialog}
+          onClose={handleCloseDelegateDialog}
+          validatorAddress={props.address}
           imageUrl={props.imageUrl}
-          name={props.name}
-          commission={props.commission}
+          validatorName={props.name}
+          validatorCommission={props.commission}
         />
-        <MenuItem onClick={handleRedelegate}> {t('validators:redelegate')}</MenuItem>
+        <MenuItem onClick={handleOpenRedelegateDialog}> {t('validators:redelegate')}</MenuItem>
         <RedelegateDialog
           open={openRedelegateDialog}
-          onClose={handleClose}
-          address={props.address}
+          onClose={handleCloseRedelegateDialog}
+          validatorAddress={props.address}
           imageUrl={props.imageUrl}
-          name={props.name}
-          commission={props.commission}
+          validatorName={props.name}
+          validatorCommission={props.commission}
         />
-        <MenuItem onClick={handleUndelegate}> {t('validators:undelegate')}</MenuItem>
+        <MenuItem onClick={handleOpenUndelegateDialog}> {t('validators:undelegate')}</MenuItem>
         <UndelegateDialog
           open={openUndelegateDialog}
-          onClose={handleClose}
-          address={props.address}
+          onClose={handleCloseUndelegateDialog}
+          validatorAddress={props.address}
           imageUrl={props.imageUrl}
-          name={props.name}
-          commission={props.commission}
+          validatorName={props.name}
+          validatorCommission={props.commission}
         />
-        <MenuItem onClick={handleClose}> {t('validators:claimRewards')}</MenuItem>
+        {/* <MenuItem onClick={handleCloseClaimRewardsDialog}> {t('validators:claimRewards')}</MenuItem> */}
       </Menu>
       <SnackBar />
     </div>
