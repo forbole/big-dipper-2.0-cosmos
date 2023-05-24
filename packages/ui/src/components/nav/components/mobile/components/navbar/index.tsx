0b921d4aab388e89f43500d175d7d0b74b4348a1
Rachel Ho <rachelhoyuenyi@gmail.com>
2023-05-24 14:34:08 +0800

    feat: hide connectwallet and walletdetails [web-cheqd] (#1254)
    ## Description
    
    Closes: #XXXX
    
    <!-- Add a description of the changes that this PR introduces and the
    files that
    are the most critical to review. -->
    
    ---
    
    ### Author Checklist
    
    _All items are required. Please add a note to the item if the item is
    not applicable and
    please add links to any relevant follow up issues._
    
    I have...
    
    - [ ] ran linting via `yarn lint`
    - [ ] wrote tests where necessary
    - [ ] included the correct [type
    prefix](https://github.com/commitizen/conventional-commit-types/blob/v3.0.0/index.json)
    in the PR title
    - [ ] targeted the correct branch
    - [ ] provided a link to the relevant issue or specification
    - [ ] reviewed "Files changed" and left comments if necessary
    - [ ] confirmed all CI checks have passed
    - [ ] added a changeset via [`yarn && yarn
    changeset`](https://github.com/changesets/changesets/blob/main/docs/adding-a-changeset.md)
    
    ---------
    
    Co-authored-by: Magic Cat <pusz.monika@gmail.com>

--- packages/ui/src/components/nav/components/mobile/components/navbar/index.tsx
+++ packages/ui/src/components/nav/components/mobile/components/navbar/index.tsx
@@ -8,8 +8,6 @@ import Link from 'next/link';
 import { useRecoilValue } from 'recoil';
 import BigDipperLogoRed from 'shared-utils/assets/big-dipper-red.svg';
 import BigDipperLogoWhite from 'shared-utils/assets/big-dipper-white.svg';
-import WalletDetails from '@/components/nav/components/wallet_details';
-import ConnectWallet from '@/components/nav/components/connect_wallet';
 
 const Navbar = (props: NavbarProps) => {
   const { classes, cx } = useStyles();
@@ -40,11 +38,6 @@ const Navbar = (props: NavbarProps) => {
           <p className="text">{selectedName}</p>
           <ExpandMoreIcon fontSize="small" />
         </div>
-        {/* =================================== */}
-        {/* Wallet Details */}
-        {/* =================================== */}
-        <WalletDetails />
-        <ConnectWallet />
         {/* =================================== */}
         {/* Hamburger */}
         {/* =================================== */}
