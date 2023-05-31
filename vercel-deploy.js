cb26bab62bcf3c618d36e0cc74076976368085a1
teamchong <25894545+teamchong@users.noreply.github.com>
2023-05-30 23:58:41 -0400

    fix: Resolve next-sitemap on vercel-deploy

--- vercel-deploy.js
+++ vercel-deploy.js
@@ -136,5 +136,7 @@ if (process.argv[2] === 'manual') {
 } else {
   /* Building the project. */
   const { name } = JSON.parse(execShell(`cat apps/web/package.json`));
-  execShell(`BASE_PATH=/ yarn workspace ${name} next build && next-sitemap`);
+  execShell(
+    `BASE_PATH=/ yarn workspace ${name} next build && BASE_PATH=/ yarn workspace ${name} next-sitemap`
+  );
 }
