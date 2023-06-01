d8f5a22e7e13e29f713215e9c8d0346809baebdf
rachelhox <rachelhoyuenyi@gmail.com>
2023-06-01 16:40:03 +0800

    kyve custom profile function

---
+++ apps/web-kyve/src/screens/home/components/blocks/components/desktop/index.tsx
@@ -0,0 +1,127 @@
+import AvatarName from '@/components/avatar_name';
+import Timestamp from '@/components/Timestamp';
+import useStyles from '@/screens/home/components/blocks/components/desktop/styles';
+import { columns } from '@/screens/home/components/blocks/components/desktop/utils';
+import type { ItemType } from '@/screens/home/components/blocks/types';
+import { getMiddleEllipsis } from '@/utils/get_middle_ellipsis';
+import { BLOCK_DETAILS } from '@/utils/go_to_page';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import { AnimatePresence, motion, Variants } from 'framer-motion';
+import useAppTranslation from '@/hooks/useAppTranslation';
+import Link from 'next/link';
+import numeral from 'numeral';
+import { FC } from 'react';
+import useKyveProfile from '@/hooks/useKyveProfile';
+
+type BlockRowProps = {
+  item: ItemType;
+};
+
+const variants: Variants = {
+  initial: {
+    height: 0,
+    display: 'flex',
+    alignItems: 'center',
+    overflow: 'hidden',
+    clipPath: 'inset(0 50 0 50)',
+  },
+  animate: {
+    height: 50,
+    display: 'flex',
+    alignItems: 'center',
+    overflow: 'hidden',
+    clipPath: 'inset(0 0 0 0)',
+  },
+  exit: {
+    height: 50,
+    display: 'flex',
+    alignItems: 'center',
+    overflow: 'hidden',
+    position: 'absolute',
+    marginTop: [50, 60],
+    opacity: 0,
+    transition: { duration: 0.5 },
+  },
+};
+
+const BlockRow: FC<BlockRowProps> = ({ item }) => {
+  const { profile } = useKyveProfile(item.consensusAddress, item.proposer);
+  const name = profile?.name ?? '';
+  const imageUrl = profile?.imageUrl ?? '';
+  const address = profile?.address ?? '';
+
+  const formattedData = {
+    height: (
+      <Link shallow prefetch={false} href={BLOCK_DETAILS(item.height)} className="value">
+        {numeral(item.height).format('0,0')}
+      </Link>
+    ),
+    txs: numeral(item.txs).format('0,0'),
+    time: <Timestamp timestamp={item.timestamp} />,
+    proposer: <AvatarName address={address} imageUrl={imageUrl} name={name} />,
+    hash: getMiddleEllipsis(item.hash, {
+      beginning: 6,
+      ending: 5,
+    }),
+  };
+  return (
+    <TableRow>
+      {columns.map((column) => {
+        const { key, align } = column;
+        return (
+          <TableCell key={`${item.hash}-${key}`} align={align}>
+            <motion.div
+              key={`${item.hash}-${key}`}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              variants={variants}
+              transition={{ duration: 1.5 }}
+            >
+              {formattedData[key as keyof typeof formattedData]}
+            </motion.div>
+          </TableCell>
+        );
+      })}
+    </TableRow>
+  );
+};
+
+type DesktopProps = {
+  className?: string;
+  items: ItemType[];
+};
+
+const Desktop: FC<DesktopProps> = ({ className, items }) => {
+  const { t } = useAppTranslation('blocks');
+  const { classes, cx } = useStyles();
+
+  return (
+    <div className={cx(classes.root, className)}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            {columns.map((column) => (
+              <TableCell key={column.key} align={column.align}>
+                {t(column.key)}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <AnimatePresence initial={false}>
+            {items.map((row) => (
+              <BlockRow key={row.hash} item={row} />
+            ))}
+          </AnimatePresence>
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default Desktop;
