a55835a4c602fb217307a5632a0d8c7264f032bf
rachelhox <rachelhoyuenyi@gmail.com>
2023-07-17 19:40:04 +0800

    add ENV to turbo json

--- packages/ui/src/components/nav/components/connect_wallet/utils.ts
+++ packages/ui/src/components/nav/components/connect_wallet/utils.ts
@@ -5,7 +5,7 @@ const { basePath } = chainConfig();
 const siteUrl = process.env.SITE_URL || `https://bigdipper.live${basePath}`;
 
 export const walletConnectRelayURL =
-  process?.env?.NEXT_PUBLIC_WALLET_CONNECT_RELAY_URL ?? 'wss://relay.walletconnect.com';
+  process?.env.NEXT_PUBLIC_WALLET_CONNECT_RELAY_URL ?? 'wss://relay.walletconnect.com';
 export const walletConnectProjectID =
   process?.env?.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID ?? '479ef4c34890ad2226193483069863c4';
 export const bigDipperURL = siteUrl ?? 'https://github.com/forbole/big-dipper-2.0-cosmos';
