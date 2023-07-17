9549fcc11ab926f4fbdd2d1770411a0f84feb032
rachelhox <rachelhoyuenyi@gmail.com>
2023-07-17 11:55:02 +0800

    web-celestia hook types fix

--- apps/web-celestia/src/screens/validators/components/list/hooks.ts
+++ apps/web-celestia/src/screens/validators/components/list/hooks.ts
@@ -1,19 +1,28 @@
 import Big from 'big.js';
 import numeral from 'numeral';
 import * as R from 'ramda';
-import { SyntheticEvent, useCallback, useState } from 'react';
+import { SyntheticEvent, useCallback, useState, useEffect } from 'react';
 import chainConfig from '@/chainConfig';
-import { useValidatorsQuery, ValidatorsQuery } from '@/graphql/types/general_types';
+import { useRecoilValue } from 'recoil';
+import { readIsUserLoggedIn } from '@/recoil/user';
+import {
+  useValidatorsQuery,
+  ValidatorsQuery,
+  useAccountDelegationsQuery,
+} from '@/graphql/types/general_types';
+import { ADDRESS_KEY } from '@/utils/localstorage';
 import { SlashingParams } from '@/models';
 import type {
   ItemType,
   ValidatorsState,
   ValidatorType,
+  ValidatorsCoinsConditionType,
 } from '@/screens/validators/components/list/types';
+import { useRewards } from '@/screens/account_details/utils';
 import { formatToken } from '@/utils/format_token';
 import { getValidatorCondition } from '@/utils/get_validator_condition';
 
-const { extra, votingPowerTokenUnit } = chainConfig();
+const { extra, votingPowerTokenUnit, primaryTokenUnit } = chainConfig();
 
 // ==========================
 // Parse data
@@ -92,6 +101,16 @@ export const useValidators = () => {
     sortKey: 'validator.name',
     sortDirection: 'asc',
   });
+  const [userAddress, setUserAddress] = useState<string>('');
+  const [delegationValidators, setDelegationValidators] = useState<ValidatorsCoinsConditionType[]>(
+    []
+  );
+  const [rewardValidators, setRewardValidators] = useState<ValidatorsCoinsConditionType[]>([]);
+  const loggedIn = useRecoilValue(readIsUserLoggedIn);
+
+  useEffect(() => {
+    setUserAddress(localStorage.getItem(ADDRESS_KEY) ?? '');
+  }, []);
 
   const handleSetState = useCallback(
     (stateChange: (prevState: ValidatorsState) => ValidatorsState) => {
@@ -104,7 +123,7 @@ export const useValidators = () => {
   );
 
   // ==========================
-  // Fetch Data
+  // Fetch Validators Data
   // ==========================
   useValidatorsQuery({
     onCompleted: (data) => {
@@ -122,6 +141,60 @@ export const useValidators = () => {
       }));
     },
   });
+
+  // ==========================
+  // Fetch Relegation Data
+  // ==========================
+  const {
+    data: delegationsData,
+    loading: delegationsLoading,
+    error: delegationsError,
+  } = useAccountDelegationsQuery({
+    variables: {
+      address: userAddress,
+      // pass the validator set params here
+      limit: 50,
+    },
+  });
+
+  useEffect(() => {
+    if (delegationsData && delegationsData.delegations && state.items && loggedIn) {
+      const {
+        delegations: { delegations },
+      } = delegationsData;
+      const delegatedValidators = delegations?.map((data) => {
+        const target = state.items.find((x) => x.validator === data.validator_address);
+        return {
+          status: target?.status ?? 0,
+          condition: target?.condition ?? 0,
+          validator: data.validator_address ?? '',
+          coins: data.coins ?? {},
+        };
+      });
+      setDelegationValidators(delegatedValidators || []);
+    }
+  }, [delegationsData, delegationsLoading, delegationsError, state.items, loggedIn]);
+
+  // ==========================
+  // Fetch Rewards Data
+  // ==========================
+  const rewards = useRewards(userAddress);
+
+  useEffect(() => {
+    if (rewards && state.items && loggedIn) {
+      const { delegationRewards } = rewards;
+      const rewardsValidators = delegationRewards?.map((data) => {
+        const target = state.items.find((x) => x.validator === data?.validatorAddress);
+        return {
+          status: target?.status ?? 0,
+          condition: target?.condition ?? 0,
+          validator: data?.validatorAddress ?? '',
+          coins: data?.coins ? data?.coins : [{ denom: primaryTokenUnit, amount: '0' }],
+        };
+      });
+      setRewardValidators(rewardsValidators || []);
+    }
+  }, [state.items, loggedIn, rewards]);
 
   const handleTabChange = useCallback(
     (_event: SyntheticEvent<Element, globalThis.Event>, newValue: number) => {
@@ -202,12 +275,35 @@ export const useValidators = () => {
     [search, state.sortDirection, state.sortKey, state.tab]
   );
 
+  const sortForbole = useCallback((items: ItemType[]) => {
+    const sorted: ItemType[] = R.clone(items);
+
+    sorted.sort((a, b) => {
+      const compareA = a.validator.name.toLowerCase();
+      const compareB = b.validator.name.toLowerCase();
+
+      if (compareA === 'forbole' && compareB !== 'forbole') {
+        return -1;
+      }
+      if (compareA !== 'forbole' && compareB === 'forbole') {
+        return 1;
+      }
+
+      return 0;
+    });
+
+    return sorted;
+  }, []);
+
   return {
     state,
+    delegationValidators,
+    rewardValidators,
     handleTabChange,
     handleSort,
     handleSearch,
     sortItems,
+    sortForbole,
     search,
   };
 };
